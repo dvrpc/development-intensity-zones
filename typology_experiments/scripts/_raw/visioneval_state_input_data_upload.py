1233f5e6790b721181fe8354a1e0cbc69994fe7c
ISchwarzenbergDVRPC <ischwarzenberg@dvrpc.org>
2022-08-31 16:41:08 -0400

    Update visioneval_state_input_data_upload.py

--- typology_experiments/scripts/_raw/visioneval_state_input_data_upload.py
+++ typology_experiments/scripts/_raw/visioneval_state_input_data_upload.py
@@ -2,16 +2,19 @@
 This script uploads the raw input data that's needed to make the VisionEval (VE)-State typology method into _raw
 """
 
-proj_library_folder_path = (
-    "C:/Users/ISchwarzenberg/Miniconda3/envs/tracking-progress/Library/share/proj"
-)
-
-
 import pandas as pd
 
 import geopandas as gpd
 
 import esri2gpd  # FIRST HAD TO RUN "conda activate typology-experiments" AND THEN 'python -m pip install "esri2gpd"' BEFORE RUNNING THIS COMMAND
+
+import gzip
+
+import requests
+
+import io
+
+import pyproj  # This and the pyproj.datadir.set_data_dir() command solve an error that came up later when the nbi_tables_creation_year shapefile was first made
 
 
 from typology_experiments import Database, DATABASE_URL
@@ -22,6 +25,11 @@ db = Database(DATABASE_URL)
 from typology_experiments.helpers.helper_functions import *  # Imports all functions from helper_functions.py. THIS MUST ALWAYS COME AFTER THE
 
 # "from typology_experiments import Database, DATABASE_URL" AND "db = Database(DATABASE_URL)" COMMANDS IN THAT ORDER
+
+
+pyproj.datadir.set_data_dir(
+    "C:/Users/ISchwarzenberg/Miniconda3/envs/tracking-progress/Library/share/proj"
+)
 
 
 api_urls = [
@@ -37,18 +45,34 @@ streaming_results_list = [
 ]  # Uses my function to stream in the census table I want using the census API. And note that because each table has a different column name for block
 
 
-b  # Get LODES7 2018 by 2010 block by Workplace (WAC) (including the total employment in all sectors column - the "JT00" GZ files have all job types) (get from https://lehd.ces.census.gov/data/#lodes )
+lodes7_urls = [
+    "https://lehd.ces.census.gov/data/lodes/LODES7/de/wac/de_wac_S000_JT00_2018.csv.gz",
+    "https://lehd.ces.census.gov/data/lodes/LODES7/md/wac/md_wac_S000_JT00_2018.csv.gz",
+    "https://lehd.ces.census.gov/data/lodes/LODES7/nj/wac/nj_wac_S000_JT00_2018.csv.gz",
+    "https://lehd.ces.census.gov/data/lodes/LODES7/pa/wac/pa_wac_S000_JT00_2018.csv.gz",
+]  # Creates a list of the URLs to use to get the LODES7 "wac_S000_JT00_2018" CSV GZ files for each state (DE, MD, NJ and PA)
 
+lodes7_data_list = [
+    pd.read_csv(gzip.open(io.BytesIO(requests.get(i).content))) for i in lodes7_urls
+]  # Streams in those LODES7 tables from their CSV GZ files
+
+
+b  # Bring in the records from the 2015 DVRPC land use inventory where lu15sub = '13000'
+
+"""
+dvrpc_land_use_2015 = gpd.read_file("https://arcgis.dvrpc.org/portal/rest/services/Planning/DVRPC_LandUse_2015/FeatureServer/0/query?f=geojson&where=(lu15sub%20IN%20(%2713000%27))&outFields=*")
 
 dvrpc_land_use_2015 = esri2gpd.get(
-    "https://arcgis.dvrpc.org/portal/rest/services/Planning/DVRPC_LandUse_2015/FeatureServer/0",
-    where="lu15sub = '13000'",
+    "https://arcgis.dvrpc.org/portal/rest/services/Planning/DVRPC_LandUse_2015/FeatureServer/0/query?f=geojson&where=(lu15sub%20IN%20(%2713000%27))&outFields=*"
 ).to_crs(
     crs="EPSG:32618"
 )  # FIX THE ERROR THAT'S HERE WHEN I COME BACK HERE: Reads in as a geo data frame/shapefile in the standard DVRPC EPSG a GIS server link containing just the records I want from the 2015 DVRPC land use inventory
+"""
 
 
-b  # Bring in entire 2020 dvrpc protected open space inventory, located at P:\15-44-070 Environmental Planning\GIS Data\Open Space Inventory\Final Layers POS 2020\2020_CompleteRegion_POS.shp
+dvrpc_pos_2020 = gpd.read_file(
+    "P:/15-44-070 Environmental Planning/GIS Data/Open Space Inventory/Final Layers POS 2020/2020_CompleteRegion_POS.shp"
+)  # FIX THE ERROR THAT'S HERE WHEN I COME BACK HERE: Brings in the entire 2020 DVRPC protected open space inventory
 
 
 b  # Bring in the features from  G:\Shared drives\Long Range Plan\2050B Plan\Centers Update\typology_experiments\Shapes\POS.gdb
