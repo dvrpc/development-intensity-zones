91cdb2118aca5706fcf162ff374015ce982ae491
ISchwarzenbergDVRPC <ischwarzenberg@dvrpc.org>
2022-09-23 09:50:12 -0400

    Update analysis.block_groups_dvrpc_2020 creation.sql

--- typology_experiments/scripts/analysis/analysis.block_groups_dvrpc_2020 creation.sql
+++ typology_experiments/scripts/analysis/analysis.block_groups_dvrpc_2020 creation.sql
@@ -2,53 +2,10 @@ drop table if exists analysis.block_groups_dvrpc_2020;
 
 create table analysis.block_groups_dvrpc_2020 as
 with
-	block_groups_state_34_2020 as (
-		select "STATEFP", 
-		"COUNTYFP", 
-		concat("STATEFP","COUNTYFP") as county_id_5dig,
-		"TRACTCE",
-		"BLKGRPCE",
-		"GEOID",
-		"NAMELSAD",
-		"ALAND",
-		"AWATER",
-		geom
-		from _raw.tl_2020_34_bg
-	),
-	block_groups_state_42_2020 as (
-		select "STATEFP", 
-		"COUNTYFP", 
-		concat("STATEFP","COUNTYFP") as county_id_5dig,
-		"TRACTCE",
-		"BLKGRPCE",
-		"GEOID",
-		"NAMELSAD",
-		"ALAND",
-		"AWATER",
-		geom
-		from _raw.tl_2020_42_bg
-	),
-	block_groups_nj_pa_2020_without_in_dvrpc_flag as (
-		select * from block_groups_state_34_2020 union
-		select * from block_groups_state_42_2020
-		),
-	dvrpc_county_id_5dig_values as (select county_id_5dig, 'Yes' as in_dvrpc from _resources.county_key),
-    block_groups_nj_pa_2020_with_in_dvrpc_flag as (
-        select
-            b."STATEFP",
-            b."COUNTYFP",
-            d.in_dvrpc,
-            b."TRACTCE",
-            b."BLKGRPCE",
-            b."GEOID",
-            b."NAMELSAD",
-            b."ALAND",
-            b."AWATER",
-            b.geom
-        from block_groups_nj_pa_2020_without_in_dvrpc_flag b
-        	left join dvrpc_county_id_5dig_values d
-            on b.county_id_5dig = d.county_id_5dig
-    )
+	block_groups_dvrpc_2020_with_potential_duplicates as (
+		select * from _raw.tl_2020_34_bg where "COUNTYFP" in ('005', '007', '015', '021') union 
+		select * from _raw.tl_2020_42_bg where "COUNTYFP" in ('017', '029', '045', '091', '101')
+	)
     
     
-    select "STATEFP", "COUNTYFP", "TRACTCE", "BLKGRPCE", "GEOID", "NAMELSAD", "ALAND", "AWATER", geom from block_groups_nj_pa_2020_with_in_dvrpc_flag where in_dvrpc='Yes'
+    select distinct on ("STATEFP", "COUNTYFP", "TRACTCE", "BLKGRPCE", "GEOID", "NAMELSAD", "ALAND", "AWATER") "STATEFP", "COUNTYFP", "TRACTCE", "BLKGRPCE", "GEOID", "NAMELSAD", "ALAND", "AWATER", geom from block_groups_dvrpc_2020_with_potential_duplicates /*FOUND OUT HOW TO GET DISTINCT ROWS OF A DATA FRAME BY ONLY CERTAIN COLUMNS FROM https://stackoverflow.com/a/16918028 (IN TURN FOUND ON https://stackoverflow.com/questions/16913969/postgres-distinct-but-only-for-one-column )*/ 
