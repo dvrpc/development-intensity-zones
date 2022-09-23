963db2a7eb54d6e9b45a12848b3da46cb649dfe0
ISchwarzenbergDVRPC <ischwarzenberg@dvrpc.org>
2022-09-23 11:05:53 -0400

    Update block_centroids_2010_with_emp_create.py

--- typology_experiments/scripts/analysis/block_centroids_2010_with_emp_create.py
+++ typology_experiments/scripts/analysis/block_centroids_2010_with_emp_create.py
@@ -6,6 +6,8 @@ import pandas as pd
 
 import geopandas as gpd
 
+import numpy as np
+
 
 from typology_experiments import Database, DATABASE_URL
 
@@ -14,7 +16,7 @@ db = Database(DATABASE_URL)
 
 blocks_all4states_2010 = db.get_geodataframe_from_query(
     "SELECT * FROM _raw.tl_2010_10_tabblock10 UNION SELECT * FROM _raw.tl_2010_24_tabblock10 UNION SELECT * FROM _raw.tl_2010_34_tabblock10 UNION SELECT * FROM _raw.tl_2010_42_tabblock10"
-)  # Uses my function to bring in the 2010 Delaware, Maryland, New Jersey and Pennsylvania blocks shapefiles, and combine them into 1 big one
+)  # Uses my function to bring in the 2010 Delaware, Maryland, New Jersey and Pennsylvania blocks shapefiles, and combines them into 1 big one
 
 blocks_all4states_2010_centroids = (
     blocks_all4states_2010.copy()
@@ -25,23 +27,54 @@ blocks_all4states_2010_centroids["geom"] = blocks_all4states_2010_centroids[
 ].centroid  # Finishes the process of getting all the blocks' centroids
 
 
-lodes_all4states_2018 = db.get_dataframe_from_query(
-    'SELECT * FROM _raw."de_wac_S000_JT00_2018" UNION SELECT * FROM _raw."md_wac_S000_JT00_2018" UNION SELECT * FROM _raw."nj_wac_S000_JT00_2018" UNION SELECT * FROM _raw."pa_wac_S000_JT00_2018"'
-)  # Uses my function to bring in the 2018 Delaware, Maryland, New Jersey and Pennsylvania LODES tables, and combine them into 1 big one
+total_jobs_from_lodes = db.get_dataframe_from_query(
+    'SELECT w_geocode, c000 AS lodes_emp FROM _raw."de_wac_S000_JT00_2018" UNION SELECT w_geocode, c000 AS lodes_emp FROM _raw."md_wac_S000_JT00_2018" UNION SELECT w_geocode, c000 AS lodes_emp FROM _raw."nj_wac_S000_JT00_2018" UNION SELECT w_geocode, c000 AS lodes_emp FROM _raw."pa_wac_S000_JT00_2018"'
+)  # Uses my function to bring in just the columns I want from the 2018 Delaware, Maryland, New Jersey and Pennsylvania LODES tables, and combines them into 1 big table
 
-lodes_all4states_2018.insert(
-    1, "GEOID10", lodes_all4states_2018["w_geocode"].apply(str)
-)  # Makes a string column of w_geocode to get each block's GEOID10 value, and puts it to the right of w_geocode
-
+total_jobs_from_lodes["GEOID10"] = total_jobs_from_lodes["w_geocode"].apply(
+    str
+)  # Makes a string column of w_geocode to get each block's GEOID10 value
 
 block_centroids_2010_with_emp = blocks_all4states_2010_centroids.merge(
-    lodes_all4states_2018, on=["GEOID10"], how="left"
-)  # Left joins lodes_all4states_2018 to blocks_all4states_2010_centroids to create block_centroids_2010_with_emp
+    total_jobs_from_lodes[["GEOID10", "lodes_emp"]], on=["GEOID10"], how="left"
+)  # Left joins just the columns I want from total_jobs_from_lodes to blocks_all4states_2010_centroids to create block_centroids_2010_with_emp
+
+
+total_jobs_from_dvrpc_emp_forecast = db.get_dataframe_from_query(
+    "SELECT block_id, total_jobs AS forecast_2020_emp FROM _raw.dvrpc_forecast_2020_emp_block10"
+)  # Uses my function to bring in the DVRPC 2020 employment forecast by 2010 block data
+
+total_jobs_from_dvrpc_emp_forecast["GEOID10"] = total_jobs_from_dvrpc_emp_forecast[
+    "block_id"
+].apply(
+    str
+)  # Makes a string column of block_id to get each block's GEOID10 value
+
+block_centroids_2010_with_emp = block_centroids_2010_with_emp.merge(
+    total_jobs_from_dvrpc_emp_forecast[["GEOID10", "forecast_2020_emp"]], on=["GEOID10"], how="left"
+)  # Left joins just the columns I want from total_jobs_from_dvrpc_emp_forecast to block_centroids_2010_with_emp so that it now has both lodes_emp and forecast_2020_emp
+
+
+block_centroids_2010_with_emp["county_id_5dig"] = (
+    block_centroids_2010_with_emp["STATEFP10"] + block_centroids_2010_with_emp["COUNTYFP10"]
+)  # Makes a new column getting each block (centroid)'s county ID
+
+dvrpc_county_id_5dig_values = [
+    s for l in db.query("SELECT county_id_5dig FROM _resources.county_key") for s in l
+]  # Uses my function to get a list of the DVRPC county census IDs
+
+block_centroids_2010_with_emp["combo_emp"] = np.where(
+    block_centroids_2010_with_emp["county_id_5dig"].isin(dvrpc_county_id_5dig_values),
+    block_centroids_2010_with_emp["forecast_2020_emp"],
+    block_centroids_2010_with_emp["lodes_emp"],
+)  # Creates combo_emp by saying if the block centroid is in a DVRPC county, use the forecast_2020_emp value, otherwise use the lodes_emp value
+
 
 block_centroids_2010_with_emp = block_centroids_2010_with_emp[
     [
         "STATEFP10",
         "COUNTYFP10",
+        "county_id_5dig",
         "TRACTCE10",
         "BLOCKCE10",
         "GEOID10",
@@ -55,63 +88,12 @@ block_centroids_2010_with_emp = block_centroids_2010_with_emp[
         "AWATER10",
         "INTPTLAT10",
         "INTPTLON10",
-        "w_geocode",
-        "c000",
-        "ca01",
-        "ca02",
-        "ca03",
-        "ce01",
-        "ce02",
-        "ce03",
-        "cns01",
-        "cns02",
-        "cns03",
-        "cns04",
-        "cns05",
-        "cns06",
-        "cns07",
-        "cns08",
-        "cns09",
-        "cns10",
-        "cns11",
-        "cns12",
-        "cns13",
-        "cns14",
-        "cns15",
-        "cns16",
-        "cns17",
-        "cns18",
-        "cns19",
-        "cns20",
-        "cr01",
-        "cr02",
-        "cr03",
-        "cr04",
-        "cr05",
-        "cr07",
-        "ct01",
-        "ct02",
-        "cd01",
-        "cd02",
-        "cd03",
-        "cd04",
-        "cs01",
-        "cs02",
-        "cfa01",
-        "cfa02",
-        "cfa03",
-        "cfa04",
-        "cfa05",
-        "cfs01",
-        "cfs02",
-        "cfs03",
-        "cfs04",
-        "cfs05",
-        "createdate",
+        "lodes_emp",
+        "forecast_2020_emp",
+        "combo_emp",
         "geom",
-        "uid",
     ]
-]  # Reorders the columns
+]  # Simultaneously keeps only the columns I want and reorders them
 
 db.import_geodataframe(
     block_centroids_2010_with_emp,
