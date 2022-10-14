7c6232e1a38f4d9e0699039411fa8f411e53bec0
ISchwarzenbergDVRPC <ischwarzenberg@dvrpc.org>
2022-10-14 09:37:02 -0400

    Update ve_default_results_table_create.py

--- typology_experiments/scripts/analysis/ve_default_results_table_create.py
+++ typology_experiments/scripts/analysis/ve_default_results_table_create.py
@@ -25,9 +25,9 @@ ve_thresholds = db.get_dataframe_from_query(
     "SELECT * FROM _resources.ve_thresholds"
 )  # Uses my function to bring in _resources.ve_thresholds
 
-data_for_area_type_column = db.get_dataframe_from_query(
+data_for_area_type_columns = db.get_dataframe_from_query(
     "SELECT accessibility_levels as accessibility_level, density_levels as density_level, area_type FROM _resources.ve_classifications"
-)  # Uses my function to bring in _resources.ve_classifications, but to use to create the eventual area_type column
+)  # Uses my function to bring in _resources.ve_classifications, but to use to create the eventual area_type columns
 
 
 density_and_accessibility_table["density_level"] = np.where(
@@ -113,13 +113,125 @@ density_and_accessibility_table["accessibility_level"] = np.where(
 )  # Repeats the process, but for creating the new accessibility_level column instead
 
 
-ve_default_results_table = pd.merge(
+density_and_accessibility_table["density_level_2050"] = np.where(
+    density_and_accessibility_table["density_2050"]
+    < ve_thresholds.loc[ve_thresholds["levels"] == "low", "density_thresholds"].iloc[0],
+    "very low",
+    np.where(
+        (
+            density_and_accessibility_table["density_2050"]
+            >= ve_thresholds.loc[ve_thresholds["levels"] == "low", "density_thresholds"].iloc[0]
+        )
+        & (
+            density_and_accessibility_table["density_2050"]
+            < ve_thresholds.loc[ve_thresholds["levels"] == "moderate", "density_thresholds"].iloc[0]
+        ),
+        "low",
+        np.where(
+            (
+                density_and_accessibility_table["density_2050"]
+                >= ve_thresholds.loc[
+                    ve_thresholds["levels"] == "moderate", "density_thresholds"
+                ].iloc[0]
+            )
+            & (
+                density_and_accessibility_table["density_2050"]
+                < ve_thresholds.loc[ve_thresholds["levels"] == "high", "density_thresholds"].iloc[0]
+            ),
+            "moderate",
+            np.where(
+                density_and_accessibility_table["density_2050"]
+                >= ve_thresholds.loc[ve_thresholds["levels"] == "high", "density_thresholds"].iloc[
+                    0
+                ],
+                "high",
+                "N/A",
+            ),
+        ),
+    ),
+)  # Repeats the process, but for creating the new density_2050 column instead
+
+density_and_accessibility_table["accessibility_level_2050"] = np.where(
+    density_and_accessibility_table["accessibility_2050"]
+    < ve_thresholds.loc[ve_thresholds["levels"] == "low", "accessibility_thresholds"].iloc[0],
+    "very low",
+    np.where(
+        (
+            density_and_accessibility_table["accessibility_2050"]
+            >= ve_thresholds.loc[ve_thresholds["levels"] == "low", "accessibility_thresholds"].iloc[
+                0
+            ]
+        )
+        & (
+            density_and_accessibility_table["accessibility_2050"]
+            < ve_thresholds.loc[
+                ve_thresholds["levels"] == "moderate", "accessibility_thresholds"
+            ].iloc[0]
+        ),
+        "low",
+        np.where(
+            (
+                density_and_accessibility_table["accessibility_2050"]
+                >= ve_thresholds.loc[
+                    ve_thresholds["levels"] == "moderate", "accessibility_thresholds"
+                ].iloc[0]
+            )
+            & (
+                density_and_accessibility_table["accessibility_2050"]
+                < ve_thresholds.loc[
+                    ve_thresholds["levels"] == "high", "accessibility_thresholds"
+                ].iloc[0]
+            ),
+            "moderate",
+            np.where(
+                density_and_accessibility_table["accessibility_2050"]
+                >= ve_thresholds.loc[
+                    ve_thresholds["levels"] == "high", "accessibility_thresholds"
+                ].iloc[0],
+                "high",
+                "N/A",
+            ),
+        ),
+    ),
+)  # Repeats the process, but for creating the new accessibility_2050 column instead
+
+
+density_and_accessibility_table = pd.merge(
     density_and_accessibility_table,
-    data_for_area_type_column,
+    data_for_area_type_columns,
     on=["accessibility_level", "density_level"],
     how="left",
-)  # Left joins data_for_area_type_column to density_and_accessibility_table to create area_type, and stores that result in a new object which contains the VisionEval default results table
+)  # Left joins data_for_area_type_columns to density_and_accessibility_table to create area_type
 
+ve_default_results_table = pd.merge(
+    density_and_accessibility_table,
+    data_for_area_type_columns.rename(
+        columns={
+            "accessibility_level": "accessibility_level_2050",
+            "density_level": "density_level_2050",
+            "area_type": "area_type_2050",
+        }
+    ),
+    on=["accessibility_level_2050", "density_level_2050"],
+    how="left",
+)  # Left joins data_for_area_type_columns with its columns renamed to accomodate for 2050 to density_and_accessibility_table to create area_type_2050, and stores that result in a new object which contains the VisionEval default results table
+
+
+ve_default_results_table = ve_default_results_table[
+    [
+        "block_group20",
+        "density",
+        "accessibility",
+        "density_level",
+        "accessibility_level",
+        "area_type",
+        "density_2050",
+        "accessibility_2050",
+        "density_level_2050",
+        "accessibility_level_2050",
+        "area_type_2050",
+    ]
+]  # Reorders the columns
 
 db.import_dataframe(
     ve_default_results_table,
