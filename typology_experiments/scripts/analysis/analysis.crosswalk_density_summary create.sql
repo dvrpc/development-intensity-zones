ad3f1a5df9fe53a12d72d130cc322c4007a72429
ISchwarzenbergDVRPC <ischwarzenberg@dvrpc.org>
2022-12-12 11:03:59 -0500

    Update analysis.crosswalk_density_summary create.sql

--- typology_experiments/scripts/analysis/analysis.crosswalk_density_summary create.sql
+++ typology_experiments/scripts/analysis/analysis.crosswalk_density_summary create.sql
@@ -5,7 +5,7 @@ with
 	bones_test_results_step1 as (select area_type, crosswalk_non_pos_water_density from analysis.bones_test_results_step1),
 	crosswalk_density_summary as (
 		
-		select area_type, avg(crosswalk_non_pos_water_density) as mean from bones_test_results_step1 
+		select area_type, avg(crosswalk_non_pos_water_density) as mean, percentile_cont(0.10) within group (order by crosswalk_non_pos_water_density asc) as percentile_10 from bones_test_results_step1 
 		
 		group by area_type
 		order by area_type
