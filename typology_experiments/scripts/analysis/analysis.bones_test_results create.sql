6425eb07e2ce38b357ec5dfccb11d8f7184c57f0
ISchwarzenbergDVRPC <ischwarzenberg@dvrpc.org>
2022-12-12 11:44:37 -0500

    Update analysis.bones_test_results create.sql

--- typology_experiments/scripts/analysis/analysis.bones_test_results create.sql
+++ typology_experiments/scripts/analysis/analysis.bones_test_results create.sql
@@ -4,7 +4,7 @@ create view analysis.bones_test_results as
 with 
 	bones_test_results_step1 as (
 		
-		select row_number, block_group20, density_bones, accessibility_bones, density_level, accessibility_level, area_type, crosswalk_non_pos_water_density, case when area_type < 6 then area_type + 1 else 7 end as area_type_plus_1, geom from analysis.bones_test_results_step1
+		select block_group20, density_bones, accessibility_bones, density_level, accessibility_level, area_type, crosswalk_non_pos_water_density, case when area_type < 6 then area_type + 1 else 7 end as area_type_plus_1, geom from analysis.bones_test_results_step1
 		
 		), /*Found out how to conditionally create a column from https://stackoverflow.com/a/19029960 (in turn found on https://stackoverflow.com/questions/19029842/if-then-else-statements-in-postgresql )*/
 	crosswalk_density_summary as (select * from analysis.crosswalk_density_summary),
@@ -28,7 +28,6 @@ with
 		),
 	bones_test_results as (
         select
-            b.row_number, 
             b.block_group20, 
             b.density_bones, 
             b.accessibility_bones, 
@@ -53,4 +52,4 @@ with
     	)
     
     
-    select * from bones_test_results
+    select row_number() over() as row_number, block_group20, density_bones, accessibility_bones, density_level, accessibility_level, area_type, crosswalk_non_pos_water_density, promo_mean, promo_10th, promo_20th, promo_30th, promo_40th, promo_50th, promo_60th, promo_70th, promo_80th, promo_90th, geom from bones_test_results
