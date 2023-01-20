54cd60535f1343ab8551025ca6d88eae3222ee8c
ISchwarzenbergDVRPC <ischwarzenberg@dvrpc.org>
2023-01-20 09:14:33 -0500

    Update block_centroids_2020_with_2020_decennial_pop_and_hhs_create.py

--- typology_experiments/scripts/analysis/block_centroids_2020_with_2020_decennial_pop_and_hhs_create.py
+++ typology_experiments/scripts/analysis/block_centroids_2020_with_2020_decennial_pop_and_hhs_create.py
@@ -16,6 +16,21 @@ blocks_all4states_2020_with_2020_decennial_pop_and_hhs = db.get_geodataframe_fro
     "SELECT * FROM _raw.tl_2020_10_tabblock20 UNION SELECT * FROM _raw.tl_2020_24_tabblock20 UNION SELECT * FROM _raw.tl_2020_34_tabblock20 UNION SELECT * FROM _raw.tl_2020_42_tabblock20"
 )  # Uses my function to bring in the 2020 Delaware, Maryland, New Jersey and Pennsylvania blocks shapefiles, and combine them into 1 big one
 
+
+blocks_all4states_2020_with_2020_decennial_pop_and_hhs.insert(
+    3, "bce20_1std", blocks_all4states_2020_with_2020_decennial_pop_and_hhs["BLOCKCE20"].str[0]
+)  # Gets the first digit of each BLOCKCE20 value, puts those values into a new column, and puts that column to the left of BLOCKCE20
+
+blocks_all4states_2020_with_2020_decennial_pop_and_hhs.insert(
+    4,
+    "block_group20",
+    blocks_all4states_2020_with_2020_decennial_pop_and_hhs["STATEFP20"]
+    + blocks_all4states_2020_with_2020_decennial_pop_and_hhs["COUNTYFP20"]
+    + blocks_all4states_2020_with_2020_decennial_pop_and_hhs["TRACTCE20"]
+    + blocks_all4states_2020_with_2020_decennial_pop_and_hhs["bce20_1std"],
+)  # Gets each block's block group ID, puts those values into a new column, and puts that column to the right of bce20_1std
+
+
 block_centroids_2020_with_2020_decennial_pop_and_hhs = (
     blocks_all4states_2020_with_2020_decennial_pop_and_hhs.copy()
 )  # Begins the process of getting all the blocks' centroids
