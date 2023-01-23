6147e36d2cc8137bb91d6bfa7f5b95185553563e
ISchwarzenbergDVRPC <ischwarzenberg@dvrpc.org>
2023-01-23 09:00:38 -0500

    Update bones_density_create.py

--- typology_experiments/scripts/analysis/bones_density_create.py
+++ typology_experiments/scripts/analysis/bones_density_create.py
@@ -1,5 +1,5 @@
 """
-This script creates analysis.bones_density_and_accessibility_step1. ALSO, REMEMBER TO FIRST CASCADE DELETE/DROP CASCADE analysis.bones_density_and_accessibility_step1 BEFORE RUNNING THIS SCRIPT, AND THEN AFTER FINISHING RUNNING THIS SCRIPT, RERUN THE SCRIPTS THAT CREATE THE VIEWS THAT DEPEND ON analysis.bones_density_and_accessibility_step1. ALSO, WHAT DISTINGUISHES analysis.bones_density_and_accessibility_step1 FROM analysis.bones_density_and_accessibility IS THAT analysis.bones_density_and_accessibility_step1 IS USED TO CREATE THE BUFFERS THAT INCORPORATE THE DELAWARE RIVER, VERSUS analysis.bones_density_and_accessibility WHICH ALREADY INCORPORATES THE DELAWARE RIVER
+This script creates analysis.bones_density. ALSO, REMEMBER TO FIRST CASCADE DELETE/DROP CASCADE analysis.bones_density BEFORE RUNNING THIS SCRIPT
 """
 
 
@@ -104,83 +104,21 @@ block_groups_24co_2020 = block_groups_24co_2020.merge(
     data_for_aland_acres_column, on=["GEOID"], how="left"
 )  # Left joins data_for_aland_acres_column to block_groups_24co_2020
 
-
 block_groups_24co_2020["density_bones"] = (
     block_groups_24co_2020["density_bones_numerator"] / block_groups_24co_2020["total_aland_acres"]
-)  # Divides the numerator for density_bones calculations column by total_aland_acres to get the eventual density_bones and accessibility_bones table's density_bones column
+)  # Divides the numerator for density_bones calculations column by total_aland_acres to get the eventual analysis.bones_density's density_bones column
 
 
-costar_property_locations.insert(
-    1, "comm_sqft_thou", costar_property_locations["rentable_building_area"] / 1000
-)  # Adds the comm_sqft_thou column to costar_property_locations, and makes it the 2nd column
-
-costar_property_locations_2mibuffers_overlay = gpd.overlay(
-    costar_property_locations,
-    block_group_centroid_buffers_24co_2020_2_mile,
-    how="intersection",
-)  # Gives each property location in costar_property_locations the GEOIDs of the 2020 block group centroid 2-mile buffers they're in (this also produces multiple records per property location, since 1 centroid can be in numerous 2-mile buffers)
-
-data_for_tot_comm_sqft_thou_2mi_column = (
-    costar_property_locations_2mibuffers_overlay.groupby(["GEOID"], as_index=False)
-    .agg({"comm_sqft_thou": "sum"})
-    .rename(columns={"comm_sqft_thou": "tot_comm_sqft_thou_2mi"})
-)  # For each 2-mile buffer 2020 GEOID (block group ID/block group), gets the total commercial square feet (in thousands), thereby creating the eventual tot_comm_sqft_thou_2mi column
-
-
-block_centroids_2020_with_2020_gq_hu_5mibuffers_overlay = gpd.overlay(
-    block_centroids_2020_with_2020_gq_hu,
-    block_group_centroid_buffers_24co_2020_5_mile,
-    how="intersection",
-)  # Gives each 2020 block centroid in block_centroids_2020_with_2020_gq_hu the GEOIDs of the 2020 block group centroid 5-mile buffers they're in (this also produces multiple records per 2010 block centroid, since 1 centroid can be in numerous 5-mile buffers)
-
-data_for_tot_gq_hu_5mi_column = (
-    block_centroids_2020_with_2020_gq_hu_5mibuffers_overlay.groupby(["GEOID"], as_index=False)
-    .agg({"gq_hu": "sum"})
-    .rename(columns={"gq_hu": "tot_gq_hu_5mi"})
-)  # For each 5-mile buffer 2020 GEOID (block group ID/block group), gets the total group quarters population and total housing units, thereby creating the eventual tot_gq_hu_5mi column
-
-data_for_accessibility_bones_columns = pd.merge(
-    data_for_tot_comm_sqft_thou_2mi_column,
-    data_for_tot_gq_hu_5mi_column,
-    on=["GEOID"],
-    how="left",
-)  # Left joins data_for_tot_gq_hu_5mi_column to data_for_tot_comm_sqft_thou_2mi_column to essentially start getting the data for the eventual accessibility_bones columns
-
-
-data_for_accessibility_bones_columns["accessibility_bones"] = (
-    (
-        data_for_accessibility_bones_columns["tot_comm_sqft_thou_2mi"]
-        * data_for_accessibility_bones_columns["tot_gq_hu_5mi"]
-    )
-    * 2
-) / (
-    data_for_accessibility_bones_columns["tot_comm_sqft_thou_2mi"]
-    + data_for_accessibility_bones_columns["tot_gq_hu_5mi"]
-)  # Creates the actual eventual accessibility_bones column
-
-data_for_accessibility_bones_columns = data_for_accessibility_bones_columns[
-    ["GEOID", "accessibility_bones"]
-]  # Just keeps the columns I want to keep and in the order I want to keep them in
-
-block_groups_24co_2020 = block_groups_24co_2020.merge(
-    data_for_accessibility_bones_columns, on=["GEOID"], how="left"
-)  # Left joins data_for_accessibility_bones_columns to block_groups_24co_2020
-
-
-bones_density_and_accessibility_step1 = pd.DataFrame(
-    block_groups_24co_2020[["GEOID", "density_bones", "accessibility_bones"]]
-).rename(
+bones_density = pd.DataFrame(block_groups_24co_2020[["GEOID", "density_bones"]]).rename(
     columns={"GEOID": "block_group20"}
-)  # Starts creating the eventual analysis.bones_density_and_accessibility_step1 by creating a new object that simultaneously keeps only the columns I want and with the names I want them from block_groups_24co_2020 and in a non-spatial way
+)  # Starts creating the eventual analysis.bones_density by creating a new object that simultaneously keeps only the columns I want and with the names I want them from block_groups_24co_2020 and in a non-spatial way
 
-bones_density_and_accessibility_step1[
-    ["density_bones", "accessibility_bones"]
-] = bones_density_and_accessibility_step1[["density_bones", "accessibility_bones"]].round(
+bones_density[["density_bones"]] = bones_density[["density_bones"]].round(
     2
-)  # Rounds density_bones and accessibility_bones to the nearest 2 decimal places
+)  # Rounds density_bones to the nearest 2 decimal places
 
 db.import_dataframe(
-    bones_density_and_accessibility_step1,
-    "analysis.bones_density_and_accessibility_step1",
+    bones_density,
+    "analysis.bones_density",
     df_import_kwargs={"if_exists": "replace", "index": False},
-)  # Exports the completed bones_density_and_accessibility_step1
+)  # Exports the completed bones_density
