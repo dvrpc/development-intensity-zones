0157e7962d0bcde631c1d18f5c0e383d20f25648
ISchwarzenbergDVRPC <ischwarzenberg@dvrpc.org>
2023-01-23 11:24:26 -0500

    Update analysis.crosswalk_density_summary create.sql

--- typology_experiments/scripts/analysis/analysis.crosswalk_density_summary create.sql
+++ typology_experiments/scripts/analysis/analysis.crosswalk_density_summary create.sql
@@ -2,20 +2,12 @@ drop view if exists analysis.crosswalk_density_summary;
 
 create view analysis.crosswalk_density_summary as 
 with 
-	bones_test_results_step3 as (select area_type, crosswalk_non_pos_water_density from analysis.bones_test_results_step3),
+	bones_test_results_step1 as (select area_type, crosswalk_non_pos_water_density from analysis.bones_test_results_step1),
 	crosswalk_density_summary as (
 		
-		select area_type, avg(crosswalk_non_pos_water_density) as mean, 
-		percentile_cont(0.10) within group (order by crosswalk_non_pos_water_density asc) as percentile_10, 
-		percentile_cont(0.20) within group (order by crosswalk_non_pos_water_density asc) as percentile_20,  
-		percentile_cont(0.30) within group (order by crosswalk_non_pos_water_density asc) as percentile_30,  
-		percentile_cont(0.40) within group (order by crosswalk_non_pos_water_density asc) as percentile_40,  
-		percentile_cont(0.50) within group (order by crosswalk_non_pos_water_density asc) as percentile_50, 
-		percentile_cont(0.60) within group (order by crosswalk_non_pos_water_density asc) as percentile_60, 
-		percentile_cont(0.70) within group (order by crosswalk_non_pos_water_density asc) as percentile_70, 
-		percentile_cont(0.80) within group (order by crosswalk_non_pos_water_density asc) as percentile_80,  
-		percentile_cont(0.90) within group (order by crosswalk_non_pos_water_density asc) as percentile_90   
-		from bones_test_results_step3 
+		select area_type, 
+		percentile_cont(0.40) within group (order by crosswalk_non_pos_water_density asc) as percentile_40  
+		from bones_test_results_step1 
 		
 		group by area_type
 		order by area_type
