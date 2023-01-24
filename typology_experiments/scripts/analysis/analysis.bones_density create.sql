738c68bd53c7d347fb48c73aa502a8cd86f87237
ISchwarzenbergDVRPC <ischwarzenberg@dvrpc.org>
2023-01-24 15:31:39 -0500

    Update analysis.bones_density create.sql

--- typology_experiments/scripts/analysis/analysis.bones_density create.sql
+++ typology_experiments/scripts/analysis/analysis.bones_density create.sql
@@ -1,6 +1,6 @@
-drop view if exists analysis.bones_density;
+drop view if exists analysis.density_index;
 
-create view analysis.bones_density as 
+create view analysis.density_index as 
 with 
 	block_groups_24co_2020 as (select "GEOID" as block_group20, aland_acres, geom from analysis.block_groups_24co_2020),
 	tot_hus_2020_bg as (
@@ -34,16 +34,16 @@ with
         	left join costarproperties_rentable_area_bg d
             on b.block_group20 = d.block_group20
     	),
-    numerators_for_density_bones_calculations_step1 as (
+    numerators_for_density_index_calculations_step1 as (
 	
 		select block_group20, sum(comm_sqft_thou) as total_comm_sqft_thou, sum(housing_units_d20) as total_housing_units from block_groups_24co_2020_with_costarproperties_rentable_area_bg_too
 		
 		group by block_group20
 		
 		),
-	numerators_for_density_bones_calculations as (
+	numerators_for_density_index_calculations as (
 	
-		select block_group20, total_comm_sqft_thou+total_housing_units as density_bones_numerator from numerators_for_density_bones_calculations_step1
+		select block_group20, total_comm_sqft_thou+total_housing_units as density_index_numerator from numerators_for_density_index_calculations_step1
 		
 		),
 	data_for_aland_acres_column as (
@@ -53,41 +53,41 @@ with
 		group by block_group20
 		
 		),
-	numerators_for_density_bones_calculations_with_total_aland_acres as (
+	numerators_for_density_index_calculations_with_total_aland_acres as (
         select
             b.block_group20,
-            b.density_bones_numerator,
+            b.density_index_numerator,
             d.total_aland_acres
-        from numerators_for_density_bones_calculations b
+        from numerators_for_density_index_calculations b
         	left join data_for_aland_acres_column d
             on b.block_group20 = d.block_group20
     	),
-    bones_density_step1 as (
+    density_index_step1 as (
     	
-    	select block_group20, density_bones_numerator/total_aland_acres as density_bones from numerators_for_density_bones_calculations_with_total_aland_acres
+    	select block_group20, density_index_numerator/total_aland_acres as density_index from numerators_for_density_index_calculations_with_total_aland_acres
     	
     	),
-	thresholds as (select levels, density_thresholds from _resources.bones_thresholds),
-	bones_density_density_level_column as (
+	thresholds as (select levels, density_index_thresholds from _resources.thresholds),
+	density_index_density_index_level_column as (
 	
-		select block_group20, 'very low' as density_level from bones_density_step1 where density_bones < (select density_thresholds from thresholds where levels = 'low') union
-		select block_group20, 'low' as density_level from bones_density_step1 where density_bones >= (select density_thresholds from thresholds where levels = 'low') and density_bones < (select density_thresholds from thresholds where levels = 'moderate') union
-		select block_group20, 'moderate' as density_level from bones_density_step1 where density_bones >= (select density_thresholds from thresholds where levels = 'moderate') and density_bones < (select density_thresholds from thresholds where levels = 'high') union
-		select block_group20, 'high' as density_level from bones_density_step1 where density_bones >= (select density_thresholds from thresholds where levels = 'high') and density_bones < (select density_thresholds from thresholds where levels = 'very high') union
-		select block_group20, 'very high' as density_level from bones_density_step1 where density_bones >= (select density_thresholds from thresholds where levels = 'very high') and density_bones < (select density_thresholds from thresholds where levels = 'extreme') union
-		select block_group20, 'extreme' as density_level from bones_density_step1 where density_bones >= (select density_thresholds from thresholds where levels = 'extreme') union
-		select block_group20, 'low' as density_level from bones_density_step1 where density_bones is null
+		select block_group20, 'very low' as density_index_level from density_index_step1 where density_index < (select density_index_thresholds from thresholds where levels = 'low') union
+		select block_group20, 'low' as density_index_level from density_index_step1 where density_index >= (select density_index_thresholds from thresholds where levels = 'low') and density_index < (select density_index_thresholds from thresholds where levels = 'moderate') union
+		select block_group20, 'moderate' as density_index_level from density_index_step1 where density_index >= (select density_index_thresholds from thresholds where levels = 'moderate') and density_index < (select density_index_thresholds from thresholds where levels = 'high') union
+		select block_group20, 'high' as density_index_level from density_index_step1 where density_index >= (select density_index_thresholds from thresholds where levels = 'high') and density_index < (select density_index_thresholds from thresholds where levels = 'very high') union
+		select block_group20, 'very high' as density_index_level from density_index_step1 where density_index >= (select density_index_thresholds from thresholds where levels = 'very high') and density_index < (select density_index_thresholds from thresholds where levels = 'extreme') union
+		select block_group20, 'extreme' as density_index_level from density_index_step1 where density_index >= (select density_index_thresholds from thresholds where levels = 'extreme') union
+		select block_group20, 'low' as density_index_level from density_index_step1 where density_index is null
 		
 		), /*Basically figured out how to retrieve a cell value using SQL from https://stackoverflow.com/a/56322459 (in turn found on https://stackoverflow.com/questions/56322358/postgresql-how-to-copy-the-value-of-a-cell-in-a-row-and-paste-it-into-another-c )*/
-	bones_density as (
+	density_index as (
         select
             b.block_group20,
-            b.density_bones,
-            d.density_level
-        from bones_density_step1 b
-        	left join bones_density_density_level_column d
+            b.density_index,
+            d.density_index_level
+        from density_index_step1 b
+        	left join density_index_density_index_level_column d
             on b.block_group20 = d.block_group20
     	)
     
     
-    select * from bones_density
+    select * from density_index
