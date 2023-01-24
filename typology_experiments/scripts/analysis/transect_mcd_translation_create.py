442132b423b4ba4359c7351a88840947214736cf
ISchwarzenbergDVRPC <ischwarzenberg@dvrpc.org>
2023-01-24 18:50:42 -0500

    Update transect_mcd_translation_create.py

--- typology_experiments/scripts/analysis/transect_mcd_translation_create.py
+++ typology_experiments/scripts/analysis/transect_mcd_translation_create.py
@@ -1,5 +1,5 @@
 """
-This script creates analysis.transect_mcd_translation
+This script creates analysis.transect_mcd_translation. AND REMEMBER TO DELETE THE CURRENT analysis.transect_mcd_translation FIRST BEFORE RERUNNING THIS SCRIPT
 """
 
 
