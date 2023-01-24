0e32d6f7e80c0c828fed591994491a3d4be87d89
ISchwarzenbergDVRPC <ischwarzenberg@dvrpc.org>
2023-01-24 16:07:38 -0500

    Update analysis.bones_test_results create.sql

--- typology_experiments/scripts/analysis/analysis.bones_test_results create.sql
+++ typology_experiments/scripts/analysis/analysis.bones_test_results create.sql
@@ -1,24 +1,24 @@
-drop view if exists analysis.bones_test_results;
+drop view if exists analysis.transect;
 
-create view analysis.bones_test_results as 
+create view analysis.transect as 
 with 
-	bones_test_results_step1 as (
+	transect_step1 as (
 		
-		select block_group20, density_bones, accessibility_bones, density_level, accessibility_level, area_type, crosswalk_non_pos_water_density, 
+		select block_group20, density_index, proximity_index, density_index_level, proximity_index_level, prelim_transect_zone, crosswalk_density, 
 		
-		case when area_type < 6 then area_type + 1 else 7 end as area_type_plus_1, 
+		case when prelim_transect_zone < 6 then prelim_transect_zone + 1 else 7 end as prelim_transect_zone_plus_1, 
 		
-		geom from analysis.bones_test_results_step1
+		geom from analysis.transect_step1
 		
 		), /*Found out how to conditionally create a column from https://stackoverflow.com/a/19029960 (in turn found on https://stackoverflow.com/questions/19029842/if-then-else-statements-in-postgresql )*/
 	crosswalk_density_summary as (select * from analysis.crosswalk_density_summary),
-	bones_test_results_additional_columns_step1 as (
+	transect_additional_columns_step1 as (
 		
-		select block_group20, area_type, area_type_plus_1, crosswalk_non_pos_water_density,
+		select block_group20, prelim_transect_zone, prelim_transect_zone_plus_1, crosswalk_density,
 		
-		case when crosswalk_non_pos_water_density > (select percentile_40 from crosswalk_density_summary where area_type = area_type_plus_1) then 1 else 0 end as promo_40th
+		case when crosswalk_density > (select percentile_40 from crosswalk_density_summary where prelim_transect_zone = prelim_transect_zone_plus_1) then 1 else 0 end as crosswalk_bonus
 		
-		from bones_test_results_step1
+		from transect_step1
 	
 		),
 	costar_number_of_stories as (select "GEOID" as block_group20, number_of_stories from analysis.costar_number_of_stories),
@@ -29,47 +29,47 @@ with
 		group by block_group20
 		
 		),
-	bones_test_results_additional_columns_step2 as (
+	transect_additional_columns_step2 as (
         select
             b.block_group20, 
-            b.area_type, 
-            b.area_type_plus_1,
-            b.crosswalk_non_pos_water_density,
-            d.average_comm_stories, /*average_comm_stories comes to the left of promo_40th*/
-            b.promo_40th,
+            b.prelim_transect_zone, 
+            b.prelim_transect_zone_plus_1,
+            b.crosswalk_density,
+            d.average_comm_stories, /*average_comm_stories comes to the left of crosswalk_bonus*/
+            b.crosswalk_bonus,
             case when average_comm_stories >= 2.95 then 1 else 0 end as stories_bonus /*Also creates stories_bonus here*/
-        from bones_test_results_additional_columns_step1 b
+        from transect_additional_columns_step1 b
         	left join average_stories_for_each_block_group d
             on b.block_group20 = d.block_group20
     	),
-	bones_test_results_additional_columns as (
+	transect_additional_columns as (
         
-		select block_group20, area_type, area_type_plus_1, crosswalk_non_pos_water_density, average_comm_stories, promo_40th, stories_bonus,
+		select block_group20, prelim_transect_zone, prelim_transect_zone_plus_1, crosswalk_density, average_comm_stories, crosswalk_bonus, stories_bonus,
 		
-		case when area_type in (1,5,6) then area_type else area_type + promo_40th + stories_bonus end as crosswalk_promo_area_type
+		case when prelim_transect_zone in (1,5,6) then prelim_transect_zone else prelim_transect_zone + crosswalk_bonus + stories_bonus end as crosswalk_promo_prelim_transect_zone
 		
-		from bones_test_results_additional_columns_step2
+		from transect_additional_columns_step2
 		
 		),
-	bones_test_results as (
+	transect as (
         select
             b.block_group20, 
-            b.density_bones, 
-            b.accessibility_bones, 
-            b.density_level, 
-            b.accessibility_level, 
-            b.area_type, 
-            b.crosswalk_non_pos_water_density,
+            b.density_index, 
+            b.proximity_index, 
+            b.density_index_level, 
+            b.proximity_index_level, 
+            b.prelim_transect_zone, 
+            b.crosswalk_density,
             d.average_comm_stories, 
-            d.promo_40th,
+            d.crosswalk_bonus,
             d.stories_bonus,
-            d.crosswalk_promo_area_type,
+            d.crosswalk_promo_prelim_transect_zone,
 			b.geom
-        from bones_test_results_step1 b
-        	left join bones_test_results_additional_columns d
+        from transect_step1 b
+        	left join transect_additional_columns d
             on b.block_group20 = d.block_group20
             order by block_group20
     	)
     
     
-    select row_number() over() as row_number, block_group20, density_bones, accessibility_bones, density_level, accessibility_level, area_type, crosswalk_non_pos_water_density, average_comm_stories, promo_40th, stories_bonus, crosswalk_promo_area_type, geom from bones_test_results
+    select row_number() over() as row_number, block_group20, density_index, proximity_index, density_index_level, proximity_index_level, prelim_transect_zone, crosswalk_density, average_comm_stories, crosswalk_bonus, stories_bonus, crosswalk_promo_prelim_transect_zone, geom from transect
