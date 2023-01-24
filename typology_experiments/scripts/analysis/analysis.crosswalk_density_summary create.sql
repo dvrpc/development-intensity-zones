1736658f68656913ae2d52b66d5d316ccf92f431
ISchwarzenbergDVRPC <ischwarzenberg@dvrpc.org>
2023-01-24 16:02:25 -0500

    Update analysis.crosswalk_density_summary create.sql

--- typology_experiments/scripts/analysis/analysis.crosswalk_density_summary create.sql
+++ typology_experiments/scripts/analysis/analysis.crosswalk_density_summary create.sql
@@ -2,15 +2,15 @@ drop view if exists analysis.crosswalk_density_summary;
 
 create view analysis.crosswalk_density_summary as 
 with 
-	bones_test_results_step1 as (select area_type, crosswalk_non_pos_water_density from analysis.bones_test_results_step1),
+	transect_step1 as (select prelim_transect_zone, crosswalk_density from analysis.transect_step1),
 	crosswalk_density_summary as (
 		
-		select area_type, 
-		percentile_cont(0.40) within group (order by crosswalk_non_pos_water_density asc) as percentile_40  
-		from bones_test_results_step1 
+		select prelim_transect_zone, 
+		percentile_cont(0.40) within group (order by crosswalk_density asc) as percentile_40  
+		from transect_step1 
 		
-		group by area_type
-		order by area_type
+		group by prelim_transect_zone
+		order by prelim_transect_zone
 		
 		) /*Found out how to get the percentile of a column from https://popsql.com/learn-sql/postgresql/how-to-calculate-percentiles-in-postgresql */
     
