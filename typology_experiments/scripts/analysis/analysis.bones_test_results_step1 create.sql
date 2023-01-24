78afa8ea1cb906708cd9a7fa4253f152faada2c6
ISchwarzenbergDVRPC <ischwarzenberg@dvrpc.org>
2023-01-24 12:28:49 -0500

    Update analysis.bones_test_results_step1 create.sql

--- typology_experiments/scripts/analysis/analysis.bones_test_results_step1 create.sql
+++ typology_experiments/scripts/analysis/analysis.bones_test_results_step1 create.sql
@@ -2,91 +2,68 @@ drop view if exists analysis.bones_test_results_step1;
 
 create view analysis.bones_test_results_step1 as 
 with 
-	density_and_accessibility_table_without_crosswalk_density_column as (
-		
-		select block_group20, density_bones, accessibility_bones from analysis.bones_density_and_accessibility
-		
-		),
-	crosswalk_density_column as (
-		
-		select "GEOID" as block_group20, crosswalk_non_pos_water_density from analysis.crosswalks_density_block_groups_dvrpc_2020
-		
-		),
-	density_and_accessibility_table as (
+	bones_density_and_accessibility as (
         select
             b.block_group20,
             b.density_bones,
-            b.accessibility_bones,
-            d.crosswalk_non_pos_water_density
-        from density_and_accessibility_table_without_crosswalk_density_column b
-        	left join crosswalk_density_column d
-            on b.block_group20 = d.block_group20
-    	),
-	thresholds as (select * from _resources.bones_thresholds),
-	data_for_area_type_column as (select accessibility_levels as accessibility_level, density_levels as density_level, area_type from _resources.bones_classifications),
-	block_groups_24co_2020_shp as (select "GEOID" as block_group20, geom from analysis.block_groups_24co_2020),
-	bones_test_results_step1_density_level_column as (
-	
-		select block_group20, density_bones, accessibility_bones, crosswalk_non_pos_water_density, 'very low' as density_level from density_and_accessibility_table where density_bones < (select density_thresholds from thresholds where levels = 'low') union
-		select block_group20, density_bones, accessibility_bones, crosswalk_non_pos_water_density, 'low' as density_level from density_and_accessibility_table where density_bones >= (select density_thresholds from thresholds where levels = 'low') and density_bones < (select density_thresholds from thresholds where levels = 'moderate') union
-		select block_group20, density_bones, accessibility_bones, crosswalk_non_pos_water_density, 'moderate' as density_level from density_and_accessibility_table where density_bones >= (select density_thresholds from thresholds where levels = 'moderate') and density_bones < (select density_thresholds from thresholds where levels = 'high') union
-		select block_group20, density_bones, accessibility_bones, crosswalk_non_pos_water_density, 'high' as density_level from density_and_accessibility_table where density_bones >= (select density_thresholds from thresholds where levels = 'high') and density_bones < (select density_thresholds from thresholds where levels = 'very high') union
-		select block_group20, density_bones, accessibility_bones, crosswalk_non_pos_water_density, 'very high' as density_level from density_and_accessibility_table where density_bones >= (select density_thresholds from thresholds where levels = 'very high') and density_bones < (select density_thresholds from thresholds where levels = 'extreme') union
-		select block_group20, density_bones, accessibility_bones, crosswalk_non_pos_water_density, 'extreme' as density_level from density_and_accessibility_table where density_bones >= (select density_thresholds from thresholds where levels = 'extreme') union
-		select block_group20, density_bones, accessibility_bones, crosswalk_non_pos_water_density, 'low' as density_level from density_and_accessibility_table where density_bones is null
-		
-		), /*Note that the density_bones, accessibility_bones, and crosswalk_non_pos_water_density columns are brought up here only so that they can be carried over throughout the rest of the script, no operations are done on or related to those columns here. Also, basically figured out how to retrieve a cell value using SQL from https://stackoverflow.com/a/56322459 (in turn found on https://stackoverflow.com/questions/56322358/postgresql-how-to-copy-the-value-of-a-cell-in-a-row-and-paste-it-into-another-c )*/
-	block_groups_24co_2020_with_density_level_column as (
-        select
-            b.block_group20,
-            d.density_bones,
-            d.accessibility_bones,
-            d.density_level,
-            d.crosswalk_non_pos_water_density,
-            b.geom
-        from block_groups_24co_2020_shp b
-        	left join bones_test_results_step1_density_level_column d
-            on b.block_group20 = d.block_group20
-    	),
-	bones_test_results_step1_accessibility_level_column as (
-	
-		select block_group20, 'very low' as accessibility_level from density_and_accessibility_table where accessibility_bones < (select accessibility_thresholds from thresholds where levels = 'low') union
-		select block_group20, 'low' as accessibility_level from density_and_accessibility_table where accessibility_bones >= (select accessibility_thresholds from thresholds where levels = 'low') and accessibility_bones < (select accessibility_thresholds from thresholds where levels = 'moderate') union
-		select block_group20, 'moderate' as accessibility_level from density_and_accessibility_table where accessibility_bones >= (select accessibility_thresholds from thresholds where levels = 'moderate') and accessibility_bones < (select accessibility_thresholds from thresholds where levels = 'high') union
-		select block_group20, 'high' as accessibility_level from density_and_accessibility_table where accessibility_bones >= (select accessibility_thresholds from thresholds where levels = 'high') and accessibility_bones < (select accessibility_thresholds from thresholds where levels = 'very high') union
-		select block_group20, 'very high' as accessibility_level from density_and_accessibility_table where accessibility_bones >= (select accessibility_thresholds from thresholds where levels = 'very high') and accessibility_bones < (select accessibility_thresholds from thresholds where levels = 'extreme') union
-		select block_group20, 'extreme' as accessibility_level from density_and_accessibility_table where accessibility_bones >= (select accessibility_thresholds from thresholds where levels = 'extreme') union
-		select block_group20, 'low' as accessibility_level from density_and_accessibility_table where accessibility_bones is null
-		
-		),
-	block_groups_24co_2020_with_accessibility_level_column_too as (
-        select
-            b.block_group20,
-            b.density_bones,
-            b.accessibility_bones,
+            d.accessibility_bones, /*Put the "_bones" columns next to each other and the "_level" columns next to each other*/
             b.density_level,
-            d.accessibility_level,
-            b.crosswalk_non_pos_water_density,
-            b.geom
-        from block_groups_24co_2020_with_density_level_column b
-        	left join bones_test_results_step1_accessibility_level_column d
+            d.accessibility_level
+        from analysis.bones_density b
+        	left join analysis.bones_accessibility d
             on b.block_group20 = d.block_group20
     	),
-    block_groups_24co_2020_with_area_type_column_too as (
+    data_for_area_type_column as (
+    	
+    	select accessibility_levels as accessibility_level, density_levels as density_level, area_type from _resources.bones_classifications
+    	
+    	),
+    bones_density_and_accessibility_with_area_type_column as (
         select
             b.block_group20,
             b.density_bones,
             b.accessibility_bones,
             b.density_level,
             b.accessibility_level,
-            d.area_type,
-            b.crosswalk_non_pos_water_density,
-            b.geom
-        from block_groups_24co_2020_with_accessibility_level_column_too b
+            d.area_type
+        from bones_density_and_accessibility b
         	left join data_for_area_type_column d
             on b.accessibility_level = d.accessibility_level
             and b.density_level = d.density_level
+    	),
+    bones_density_and_accessibility_crosswalk_density_column as (
+		
+		select "GEOID" as block_group20, crosswalk_non_pos_water_density from analysis.crosswalks_density_block_groups_dvrpc_2020
+		
+		),
+	bones_density_and_accessibility_with_crosswalk_density_column_too as (
+        select
+            b.block_group20,
+            b.density_bones,
+            b.accessibility_bones,
+            b.density_level,
+            b.accessibility_level,
+            b.area_type,
+            d.crosswalk_non_pos_water_density
+        from bones_density_and_accessibility_with_area_type_column b
+        	left join bones_density_and_accessibility_crosswalk_density_column d
+            on b.block_group20 = d.block_group20
+    	),
+    bones_density_and_accessibility_geometries as (select "GEOID" as block_group20, geom from analysis.block_groups_24co_2020),
+	bones_density_and_accessibility_with_geometries_too as (
+        select
+            b.block_group20,
+            b.density_bones,
+            b.accessibility_bones,
+            b.density_level,
+            b.accessibility_level,
+            b.area_type,
+            b.crosswalk_non_pos_water_density,
+            d.geom
+        from bones_density_and_accessibility_with_crosswalk_density_column_too b
+        	left join bones_density_and_accessibility_geometries d
+            on b.block_group20 = d.block_group20
     	)
     
     
-    select row_number() over() as row_number, block_group20, density_bones, accessibility_bones, density_level, accessibility_level, area_type, crosswalk_non_pos_water_density, geom from block_groups_24co_2020_with_area_type_column_too
+    select row_number() over() as row_number, block_group20, density_bones, accessibility_bones, density_level, accessibility_level, area_type, crosswalk_non_pos_water_density, geom from bones_density_and_accessibility_with_geometries_too
