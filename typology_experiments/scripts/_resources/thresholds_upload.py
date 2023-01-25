1706c31d83760aa62238c139b6a75e4fbade5461
ISchwarzenbergDVRPC <ischwarzenberg@dvrpc.org>
2023-01-25 09:27:15 -0500

    Create thresholds_upload.py

---
+++ typology_experiments/scripts/_resources/thresholds_upload.py
@@ -0,0 +1,25 @@
+"""
+This script uploads _resources.thresholds. AND REMEMBER TO FIRST DROP CASCADE _resources.thresholds BEFORE RERUNNING THIS
+"""
+
+import pandas as pd
+
+
+from typology_experiments import Database, DATABASE_URL
+
+db = Database(DATABASE_URL)
+
+
+thresholds = pd.read_csv(
+    "G:/Shared drives/Long Range Plan/2050B Plan/Centers Update/typology_experiments/thresholds.csv"
+)  # Brings in the thresholds CSV I madee
+
+thresholds = thresholds.astype(
+    {"density_index_thresholds": "float", "proximity_index_thresholds": "float"}
+)  # Makes density_index_thresholds and proximity_index_thresholds float
+
+db.import_dataframe(
+    thresholds,
+    "_resources.thresholds",
+    df_import_kwargs={"if_exists": "replace", "index": False},
+)  # Exports thresholds
