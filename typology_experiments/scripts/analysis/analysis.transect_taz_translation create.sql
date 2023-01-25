a03a5ade7cde477d5f6480dd95daa62385b58cbd
ISchwarzenbergDVRPC <ischwarzenberg@dvrpc.org>
2023-01-25 17:17:12 -0500

    Update analysis.transect_taz_translation create.sql

--- typology_experiments/scripts/analysis/analysis.transect_taz_translation create.sql
+++ typology_experiments/scripts/analysis/analysis.transect_taz_translation create.sql
@@ -3,11 +3,12 @@ drop view if exists analysis.transect_taz_translation;
 create view analysis.transect_taz_translation as 
 with 
 	block2020_parent_geos as (select block_group20_id, taz_id, aland from _resources.block2020_parent_geos),
-	transect_zones as (select block_group20 as block_group20_id, transect_zone from analysis.transect),
-	block2020_parent_geos_with_transect_zone as (
+	transect_zones as (select block_group20 as block_group20_id, transect_zone, transect_zone_name from analysis.transect),
+	block2020_parent_geos_with_transect_zone_info as (
         select
             b.taz_id, 
             d.transect_zone,
+            d.transect_zone_name,
             b.aland
         from block2020_parent_geos b
         	left join transect_zones d
@@ -21,33 +22,32 @@ with
     	
     	round(cast(sum(transect_zone * aland) / sum(aland) as numeric), 0) as transect_zone
     	
-    	from block2020_parent_geos_with_transect_zone
+    	from block2020_parent_geos_with_transect_zone_info
 
     	group by taz_id
     	
     	), /*Found out how to manually calculate weighted average of one column by another from https://stackoverflow.com/a/40078094 (in turn found on https://stackoverflow.com/questions/40078047/sql-weighted-average )*/
-	taz_2010_mcdaligned as (select tazt as taz_id, shape as geom from _raw.taz_2010_mcdaligned where dvrpc='yes'),
-	transect_taz_translation_without_transect_zone_names as (
+	transect_weighted_averages_with_transect_zone_name as (
         select
             b.taz_id, 
-            d.transect_weighted_average,
-            d.transect_zone,
-            b.geom
-        from taz_2010_mcdaligned b
-        	left join transect_weighted_averages d
-            on b.taz_id = d.taz_id
+            b.transect_weighted_average,
+            b.transect_zone,
+            d.transect_zone_name
+        from transect_weighted_averages b
+        	left join block2020_parent_geos_with_transect_zone_info d
+            on b.transect_zone = d.transect_zone
     	),
-	transect_zone_names as (select * from _resources.transect_zone_names),
+	taz_2010_mcdaligned as (select tazt as taz_id, shape as geom from _raw.taz_2010_mcdaligned where dvrpc='yes'),
 	transect_taz_translation as (
         select
             b.taz_id, 
             b.transect_weighted_average,
             b.transect_zone,
-            d.transect_zone_name,
-            b.geom
-        from transect_taz_translation_without_transect_zone_names b
-        	left join transect_zone_names d
-            on b.transect_zone = d.transect_zone
+            b.transect_zone_name,
+            d.geom
+        from transect_weighted_averages_with_transect_zone_name b
+        	left join taz_2010_mcdaligned d
+            on b.taz_id = d.taz_id
     	)
     
     
