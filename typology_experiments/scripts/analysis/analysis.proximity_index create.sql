072d442ddee08666e90fd17811d5ac157effe3f8
ISchwarzenbergDVRPC <ischwarzenberg@dvrpc.org>
2023-02-01 19:48:20 -0500

    Update analysis.proximity_index create.sql

--- typology_experiments/scripts/analysis/analysis.proximity_index create.sql
+++ typology_experiments/scripts/analysis/analysis.proximity_index create.sql
@@ -2,23 +2,28 @@ drop view if exists analysis.proximity_index;
 
 create view analysis.proximity_index as 
 with 
-	proximity_index_level_column as (
-	
-		select block_group20, 'very low' as proximity_index_level from analysis.proximity_index_step1 where proximity_index < (select proximity_index_thresholds from _resources.thresholds where levels = 'low') union
-		select block_group20, 'low' as proximity_index_level from analysis.proximity_index_step1 where proximity_index >= (select proximity_index_thresholds from _resources.thresholds where levels = 'low') and proximity_index < (select proximity_index_thresholds from _resources.thresholds where levels = 'moderate') union
-		select block_group20, 'moderate' as proximity_index_level from analysis.proximity_index_step1 where proximity_index >= (select proximity_index_thresholds from _resources.thresholds where levels = 'moderate') and proximity_index < (select proximity_index_thresholds from _resources.thresholds where levels = 'high') union
-		select block_group20, 'high' as proximity_index_level from analysis.proximity_index_step1 where proximity_index >= (select proximity_index_thresholds from _resources.thresholds where levels = 'high') and proximity_index < (select proximity_index_thresholds from _resources.thresholds where levels = 'very high') union
-		select block_group20, 'very high' as proximity_index_level from analysis.proximity_index_step1 where proximity_index >= (select proximity_index_thresholds from _resources.thresholds where levels = 'very high') and proximity_index < (select proximity_index_thresholds from _resources.thresholds where levels = 'extreme') union
-		select block_group20, 'extreme' as proximity_index_level from analysis.proximity_index_step1 where proximity_index >= (select proximity_index_thresholds from _resources.thresholds where levels = 'extreme') union
-		select block_group20, null as proximity_index_level from analysis.proximity_index_step1 where proximity_index is null
-		
-		) /*Basically figured out how to retrieve a cell value using SQL from https://stackoverflow.com/a/56322459 (in turn found on https://stackoverflow.com/questions/56322358/postgresql-how-to-copy-the-value-of-a-cell-in-a-row-and-paste-it-into-another-c )*/
-	
-		
-	select
-        b.block_group20,
-        b.proximity_index,
-        d.proximity_index_level
-    from analysis.proximity_index_step1 b
-    	left join proximity_index_level_column d
-        on b.block_group20 = d.block_group20
+	proximity_index_step1 as (
+    	
+    	select block_group20, proximity_index,
+    	
+    	case when proximity_index < (select proximity_index_thresholds from _resources.thresholds where levels = 'low') then 'very low' else null end as very_low,
+    	case when proximity_index >= (select proximity_index_thresholds from _resources.thresholds where levels = 'low') and proximity_index < (select proximity_index_thresholds from _resources.thresholds where levels = 'moderate') then 'low' else null end as low,
+    	case when proximity_index >= (select proximity_index_thresholds from _resources.thresholds where levels = 'moderate') and proximity_index < (select proximity_index_thresholds from _resources.thresholds where levels = 'high') then 'moderate' else null end as moderate,
+    	case when proximity_index >= (select proximity_index_thresholds from _resources.thresholds where levels = 'high') and proximity_index < (select proximity_index_thresholds from _resources.thresholds where levels = 'very high') then 'high' else null end as high,
+    	case when proximity_index >= (select proximity_index_thresholds from _resources.thresholds where levels = 'very high') and proximity_index < (select proximity_index_thresholds from _resources.thresholds where levels = 'extreme') then 'very high' else null end as very_high,
+    	case when proximity_index >= (select proximity_index_thresholds from _resources.thresholds where levels = 'extreme') then 'extreme' else null end as extreme,
+    	case when proximity_index is null then 'null' else null end as "null"
+    	
+    	from analysis.proximity_index_step1
+    	
+    	),
+    proximity_index_step2 as (
+    	
+    	select block_group20, proximity_index, concat(very_low,low,moderate,high,very_high,extreme,"null") as proximity_index_level_step1 from proximity_index_step1
+    	
+    	)
+
+    	
+	select block_group20, proximity_index, 
+	case when proximity_index_level_step1='null' then null else proximity_index_level_step1 end as proximity_index_level
+	from proximity_index_step2
