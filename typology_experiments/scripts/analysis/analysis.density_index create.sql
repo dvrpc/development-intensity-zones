8d0716d9338a0fb497832bfed6d37dd17a774b78
ISchwarzenbergDVRPC <ischwarzenberg@dvrpc.org>
2023-02-01 19:40:44 -0500

    Update analysis.density_index create.sql

--- typology_experiments/scripts/analysis/analysis.density_index create.sql
+++ typology_experiments/scripts/analysis/analysis.density_index create.sql
@@ -1,6 +1,6 @@
-drop view if exists analysis.density_index;
+/*drop view if exists analysis.density_index;
 
-create view analysis.density_index as
+create view analysis.density_index as*/
 with 
 	block_groups_24co_2020 as (select "GEOID" as block_group20, aland_acres from analysis.block_groups_24co_2020),
 	tot_hus_2020_bg as (
@@ -46,23 +46,31 @@ with
     	from density_index_step1
     	
     	),
-	density_index_level_column as (
-	
-		select block_group20, 'very low' as density_index_level from density_index_step2 where density_index < (select density_index_thresholds from _resources.thresholds where levels = 'low') union
-		select block_group20, 'low' as density_index_level from density_index_step2 where density_index >= (select density_index_thresholds from _resources.thresholds where levels = 'low') and density_index < (select density_index_thresholds from _resources.thresholds where levels = 'moderate') union
-		select block_group20, 'moderate' as density_index_level from density_index_step2 where density_index >= (select density_index_thresholds from _resources.thresholds where levels = 'moderate') and density_index < (select density_index_thresholds from _resources.thresholds where levels = 'high') union
-		select block_group20, 'high' as density_index_level from density_index_step2 where density_index >= (select density_index_thresholds from _resources.thresholds where levels = 'high') and density_index < (select density_index_thresholds from _resources.thresholds where levels = 'very high') union
-		select block_group20, 'very high' as density_index_level from density_index_step2 where density_index >= (select density_index_thresholds from _resources.thresholds where levels = 'very high') and density_index < (select density_index_thresholds from _resources.thresholds where levels = 'extreme') union
-		select block_group20, 'extreme' as density_index_level from density_index_step2 where density_index >= (select density_index_thresholds from _resources.thresholds where levels = 'extreme') union
-		select block_group20, null as density_index_level from density_index_step2 where density_index is null
-		
-		) /*Basically figured out how to retrieve a cell value using SQL from https://stackoverflow.com/a/56322459 (in turn found on https://stackoverflow.com/questions/56322358/postgresql-how-to-copy-the-value-of-a-cell-in-a-row-and-paste-it-into-another-c )*/
-	
-	
+	density_index_step3 as (
+    	
+    	select block_group20, density_index,
+    	
+    	case when density_index < (select density_index_thresholds from _resources.thresholds where levels = 'low') then 'very low' else null end as very_low,
+    	case when density_index >= (select density_index_thresholds from _resources.thresholds where levels = 'low') and density_index < (select density_index_thresholds from _resources.thresholds where levels = 'moderate') then 'low' else null end as low,
+    	case when density_index >= (select density_index_thresholds from _resources.thresholds where levels = 'moderate') and density_index < (select density_index_thresholds from _resources.thresholds where levels = 'high') then 'moderate' else null end as moderate,
+    	case when density_index >= (select density_index_thresholds from _resources.thresholds where levels = 'high') and density_index < (select density_index_thresholds from _resources.thresholds where levels = 'very high') then 'high' else null end as high,
+    	case when density_index >= (select density_index_thresholds from _resources.thresholds where levels = 'very high') and density_index < (select density_index_thresholds from _resources.thresholds where levels = 'extreme') then 'very high' else null end as very_high,
+    	case when density_index >= (select density_index_thresholds from _resources.thresholds where levels = 'extreme') then 'extreme' else null end as extreme,
+    	case when density_index is null then 'null' else null end as "null"
+    	
+    	from density_index_step2
+    	
+    	),
+    density_index as (
+    	
+    	select block_group20, density_index, concat(very_low,low,moderate,high,very_high,extreme,"null") as density_index_level from density_index_step3
+    	
+    	)select * from density_index where density_index_level='null'/*
+    	
     select
         b.block_group20,
         b.density_index,
         d.density_index_level
     from density_index_step2 b
     	left join density_index_level_column d
-        on b.block_group20 = d.block_group20
+        on b.block_group20 = d.block_group20*/
