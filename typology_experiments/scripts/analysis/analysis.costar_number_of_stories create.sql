b8d0401d46a79ceed739b3dbf919d4a457d6fd46
ISchwarzenbergDVRPC <ischwarzenberg@dvrpc.org>
2023-02-02 09:31:08 -0500

    Update analysis.costar_number_of_stories create.sql

--- typology_experiments/scripts/analysis/analysis.costar_number_of_stories create.sql
+++ typology_experiments/scripts/analysis/analysis.costar_number_of_stories create.sql
@@ -2,45 +2,34 @@ drop table if exists analysis.costar_number_of_stories;
 
 create table analysis.costar_number_of_stories as
 with
-	costar_properties_dvrpc as (
-		
-		select propertyid, 
-		number_of_stories, 
-		building_status,
-		year_built,
-		shape as geom
-		
-		from _raw.costarproperties
-		
-		where number_of_stories is not null
-		and building_status in ('Converted', 'Existing', 'Under Construction', 'Under Renovation')
-		and year_built <= 2020
-		
-		),
-	costar_properties_surrounding_counties as (
-		
-		select "PropertyID" as propertyid,
-		"Number_Of_Stories" as number_of_stories, 
-		"Building_Status" as building_status,
-		"Year_Built" as year_built,
-		geom
-		
-		from _raw."CoStarProperties_Surrounding_Co_2023_01"
-		
-		where "Number_Of_Stories" is not null
-		and "Building_Status" in ('Converted', 'Existing', 'Under Construction', 'Under Renovation')
-		and "Year_Built" <= 2020
-		
-		),
 	costar_number_of_stories_without_bg_2020_geoids as (
 		
-		select * from costar_properties_dvrpc union
-		select * from costar_properties_surrounding_counties
+		select 
+			propertyid, 
+			number_of_stories, 
+			building_status, 
+			year_built, 
+			shape as geom 
+			from _raw.costarproperties 
+			where number_of_stories is not null 
+			and building_status in ('Converted', 'Existing', 'Under Construction', 'Under Renovation') 
+			and year_built <= 2020 union
 		
-		),
-	bg_2020_geoids as (select "GEOID", geom from analysis.block_groups_24co_2020),
-	costar_number_of_stories as (
-		select
+		select 
+			"PropertyID" as propertyid, 
+			"Number_Of_Stories" as number_of_stories, 
+			"Building_Status" as building_status, 
+			"Year_Built" as year_built, 
+			geom 
+			from _raw."CoStarProperties_Surrounding_Co_2023_01" 
+			where "Number_Of_Stories" is not null 
+			and "Building_Status" in ('Converted', 'Existing', 'Under Construction', 'Under Renovation') 
+			and "Year_Built" <= 2020
+		
+		)
+
+	
+	select
 			b.propertyid,
 			d."GEOID",
 			b.number_of_stories,
@@ -49,12 +38,8 @@ with
 			b.geom
 		from
 			costar_number_of_stories_without_bg_2020_geoids b,
-			bg_2020_geoids d
+			analysis.block_groups_24co_2020 d
 		where
 			st_intersects(b.geom, d.geom) /*Basically found out how to spatial join in SQL from Sean Lawrence*/
 		order by
-			propertyid 
-		)
-		
-
-select * from costar_number_of_stories
+			propertyid
