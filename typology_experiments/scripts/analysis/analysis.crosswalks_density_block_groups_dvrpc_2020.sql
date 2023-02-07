83b453a3b70069bc0fdcc2959e54da057f51580c
ISchwarzenbergDVRPC <ischwarzenberg@dvrpc.org>
2023-02-07 09:31:31 -0500

    Update analysis.crosswalks_density_block_groups_dvrpc_2020.sql

--- typology_experiments/scripts/analysis/analysis.crosswalks_density_block_groups_dvrpc_2020.sql
+++ typology_experiments/scripts/analysis/analysis.crosswalks_density_block_groups_dvrpc_2020.sql
@@ -1,18 +1,10 @@
 drop table if exists analysis.crosswalks_density_block_groups_dvrpc_2020;
 create table analysis.crosswalks_density_block_groups_dvrpc_2020 as
-select 
-ula."STATEFP",
-ula."COUNTYFP",
-ula."TRACTCE",
-ula."BLKGRPCE",
+select
 ula."GEOID",
-ula."NAMELSAD",
-ula."ALAND",
-ula."AWATER",
 ula.non_pos_water_acres,
 ula.aland_acres,
 ula.geom,
-ula.uid,
 cbgd.crosswalk_count / ula.aland_acres as crosswalk_aland_density,
 cbgd.crosswalk_count / ula.non_pos_water_acres as crosswalk_density
 from analysis.unprotected_land_area ula 
