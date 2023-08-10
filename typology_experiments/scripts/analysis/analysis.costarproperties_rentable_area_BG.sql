5336f29cb3ae718d65127b315e282abfef155d4c
ISchwarzenbergDVRPC <ischwarzenberg@dvrpc.org>
2023-08-10 11:32:34 -0400

    Update analysis.costarproperties_rentable_area_BG.sql

--- typology_experiments/scripts/analysis/analysis.costarproperties_rentable_area_BG.sql
+++ typology_experiments/scripts/analysis/analysis.costarproperties_rentable_area_BG.sql
@@ -1,18 +1,13 @@
-drop table if exists analysis.costarproperties_rentable_area_bg;
+drop table if exists analysis.costarproperties_rentable_area_bg cascade;
 
 create table analysis.costarproperties_rentable_area_bg as
-with
-	costarproperties_rentable_area_bg_step1 as (
-		select 
-		    bgd."GEOID",
-		    sum(crps.rentable_building_area)/1000 as commercial_sqft
-		from analysis.costarproperties_region_plus_surrounding crps
-			join analysis.block_groups_24co_2020 bgd
-		    on st_intersects(crps.geom, bgd.geom)
-			group by bgd."GEOID"
-	) /*The script up to this point was written by Brian Carney, I just changed line indents, line spacing in one instance, and replaced "costarproperties_rentable_area_BG" with "costarproperties_rentable_area_bg"*/
-	
-	
-	select * from costarproperties_rentable_area_bg_step1 where "GEOID" <> '421010369021' union
-	select "GEOID", 1901812/1000 as commercial_sqft from costarproperties_rentable_area_bg_step1 where "GEOID" = '421010369021' /*Manually updates the commercial_sqft value for that University City block group*/
-	
+select 
+    bgd."GEOID",
+    sum(crps.rentable_building_area)/1000 as commercial_sqft
+from (
+	select * from analysis.costarproperties_region_plus_surrounding union
+	select geom, rentable_building_area from _raw.not_in_costar -- AS EXAMPLES, this contains those 2 really big Costar properties, that one in University City and the GSK campus in Montco (so more similar properties can get added to this in the future if need be). _raw.not_in_costar was originally manually made by Ben Gruswitz 
+	) crps
+	join analysis.block_groups_24co_2020 bgd
+    on st_intersects(crps.geom, bgd.geom)
+	group by bgd."GEOID"  /*The script was written by Brian Carney, I just changed line indents, line spacing in one instance, replaced "costarproperties_rentable_area_BG" with "costarproperties_rentable_area_bg", and added _raw.not_in_costar into this*/
