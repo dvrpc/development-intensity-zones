c1011a22ac16a1e795067beca68b5f03e642194f
ISchwarzenbergDVRPC <ischwarzenberg@dvrpc.org>
2023-08-10 15:27:18 -0400

    Update analysis.diz_mcd create.sql

--- typology_experiments/scripts/analysis/analysis.diz_mcd create.sql
+++ typology_experiments/scripts/analysis/analysis.diz_mcd create.sql
@@ -1,18 +1,18 @@
-DROP VIEW IF EXISTS analysis.transect_mcd_translation;
+DROP VIEW IF EXISTS analysis.diz_mcd;
 
 
-CREATE VIEW analysis.transect_mcd_translation AS WITH weighted_averages AS
+CREATE VIEW analysis.diz_mcd AS WITH weighted_averages AS
     (SELECT b.mcd20_id,
-            sum(t.transect_zone * b.aland) / sum(b.aland) AS transect_weighted_average,
-            round(sum(t.transect_zone * b.aland) / sum(b.aland), 0) AS transect_zone
+            sum(t.diz_zone * b.aland) / sum(b.aland) AS diz_weighted_average,
+            round(sum(t.diz_zone * b.aland) / sum(b.aland), 0) AS diz_zone
      FROM _resources.block2020_parent_geos b
-     LEFT JOIN analysis.transect t ON b.block_group20_id = t.block_group20
+     LEFT JOIN analysis.diz_block_group t ON b.block_group20_id = t.block_group20
      GROUP BY b.mcd20_id)
 SELECT w.mcd20_id,
-       w.transect_weighted_average,
-       w.transect_zone,
-       z.transect_zone_name,
+       w.diz_weighted_average,
+       w.diz_zone,
+       z.diz_zone_name,
        d.shape AS geom
 FROM weighted_averages w
 LEFT JOIN _raw.census_mcds_2020 d ON w.mcd20_id = d.geoid::text
-LEFT JOIN _resources.transect_zone_names z ON w.transect_zone = z.transect_zone::numeric;
+LEFT JOIN _resources.diz_zone_names z ON w.diz_zone = z.diz_zone::numeric;
