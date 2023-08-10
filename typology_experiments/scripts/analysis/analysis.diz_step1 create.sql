80139cad9652b17d457365a60a4921f3891b13f8
ISchwarzenbergDVRPC <ischwarzenberg@dvrpc.org>
2023-08-10 15:13:14 -0400

    Update analysis.diz_step1 create.sql

--- typology_experiments/scripts/analysis/analysis.diz_step1 create.sql
+++ typology_experiments/scripts/analysis/analysis.diz_step1 create.sql
@@ -2,7 +2,7 @@ drop view if exists analysis.diz_step1 cascade;
 
 create view analysis.diz_step1 as 
 with 
-	density_and_proximity_indices as (
+	indices as (
         select
             b.block_group20,
             b.density_index,
@@ -13,12 +13,12 @@ with
         	left join analysis.proximity_index d
             on b.block_group20 = d.block_group20
     	),
-    data_for_prelim_diz_zone_step1_column as (
+    data_for_prelim_diz_zone_step1 as (
     	
     	select proximity_index_levels as proximity_index_level, density_index_levels as density_index_level, prelim_diz_zone as prelim_diz_zone_step1 from _resources.classifications
     	
     	),
-    density_and_proximity_indices_with_prelim_diz_zone_column_step1 as (
+    indices_with_prelim_diz_zone_step1 as (
         select
             b.block_group20,
             b.density_index,
@@ -26,26 +26,26 @@ with
             b.density_index_level,
             b.proximity_index_level,
             d.prelim_diz_zone_step1
-        from density_and_proximity_indices b
-        	left join data_for_prelim_diz_zone_step1_column d
+        from indices b
+        	left join data_for_prelim_diz_zone_step1 d
             on b.proximity_index_level = d.proximity_index_level
             and b.density_index_level = d.density_index_level
     	),
-    density_and_proximity_indices_with_prelim_diz_zone_column as (
+    indices_with_prelim_diz_zone as (
         
     	select block_group20, density_index, proximity_index, density_index_level, proximity_index_level,
     	case when block_group20 in (select "GEOID" from analysis.block_groups_24co_2020_area_calcs where percent_lu_h2o_pos >= 95) then 0 else prelim_diz_zone_step1 end as prelim_diz_zone
-    	from density_and_proximity_indices_with_prelim_diz_zone_column_step1
+    	from indices_with_prelim_diz_zone_step1
         
         ),
-    density_and_proximity_indices_crosswalk_density_column as (
+    indices_crosswalk_density as (
 		
 		select "GEOID" as block_group20, sum(crosswalk_density) as crosswalk_density from analysis.crosswalks_density_block_groups_dvrpc_2020
 		
 		group by "GEOID"
 		
 		), /*There's at least 1 but not much more than 1 block group in analysis.crosswalks_density_block_groups_dvrpc_2020 that has more than 1 polygon, so I'll sum the crosswalk density for those*/
-	density_and_proximity_indices_with_crosswalk_density_column_too as (
+	indices_with_crosswalk_density as (
         select
             b.block_group20,
             b.density_index,
@@ -54,12 +54,12 @@ with
             b.proximity_index_level,
             b.prelim_diz_zone,
             d.crosswalk_density
-        from density_and_proximity_indices_with_prelim_diz_zone_column b
-        	left join density_and_proximity_indices_crosswalk_density_column d
+        from indices_with_prelim_diz_zone b
+        	left join indices_crosswalk_density d
             on b.block_group20 = d.block_group20
     	),
-    density_and_proximity_indices_geometries as (select "GEOID" as block_group20, geom from analysis.block_groups_24co_2020),
-	density_and_proximity_indices_with_geometries_too as (
+    indices_geometries as (select "GEOID" as block_group20, geom from analysis.block_groups_24co_2020),
+	indices_with_geometries as (
         select
             b.block_group20,
             b.density_index,
@@ -69,10 +69,10 @@ with
             b.prelim_diz_zone,
             b.crosswalk_density,
             d.geom
-        from density_and_proximity_indices_with_crosswalk_density_column_too b
-        	left join density_and_proximity_indices_geometries d
+        from indices_with_crosswalk_density b
+        	left join indices_geometries d
             on b.block_group20 = d.block_group20
     	)
     
     
-    select row_number() over() as row_number, block_group20, density_index, proximity_index, density_index_level, proximity_index_level, prelim_diz_zone, crosswalk_density, geom from density_and_proximity_indices_with_geometries_too
+    select row_number() over() as row_number, block_group20, density_index, proximity_index, density_index_level, proximity_index_level, prelim_diz_zone, crosswalk_density, geom from indices_with_geometries
