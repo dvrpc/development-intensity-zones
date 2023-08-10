fe9cc9e78b694dbd3a8720eaf1d0f02feae81ac0
ISchwarzenbergDVRPC <ischwarzenberg@dvrpc.org>
2023-08-10 15:23:05 -0400

    Update analysis.diz_block_group create.sql

--- typology_experiments/scripts/analysis/analysis.diz_block_group create.sql
+++ typology_experiments/scripts/analysis/analysis.diz_block_group create.sql
@@ -1,6 +1,6 @@
-drop materialized view if exists analysis.diz cascade;
+drop materialized view if exists analysis.diz_block_group cascade;
 
-create materialized view analysis.diz as /*Found out how to create a materialized view, and what it is from https://www.postgresql.org/docs/current/rules-materializedviews.html */
+create materialized view analysis.diz_block_group as /*Found out how to create a materialized view, and what it is from https://www.postgresql.org/docs/current/rules-materializedviews.html */
 with 
 	diz_step1 as (
 		
@@ -10,15 +10,15 @@ with
 		
 		geom from analysis.diz_step1
 		
-		), /*Found out how to conditionally create a column from https://stackoverflow.com/a/19029960 (in turn found on https://stackoverflow.com/questions/19029842/if-then-else-statements-in-postgresql )*/
-	diz_acs as (
+		), --Found out how to conditionally create a column from https://stackoverflow.com/a/19029960 (in turn found on https://stackoverflow.com/questions/19029842/if-then-else-statements-in-postgresql )
+	diz_bg_av_com_s as (
 		
 		select "GEOID" as block_group20, avg(number_of_stories) as average_comm_stories from analysis.costar_number_of_stories
 		
 		group by "GEOID"
 		
-		),
-	diz_with_acs as (
+		), --Used "av_com_s" in the acronym instead of "acs" because "acs" could be easily confused with American Community Survey
+	diz_bg_with_av_com_s as (
         select
             b.block_group20, 
             b.density_index, 
@@ -31,7 +31,7 @@ with
             d.average_comm_stories,
 			b.geom
         from diz_step1 b
-        	left join diz_acs d
+        	left join diz_bg_av_com_s d
             on b.block_group20 = d.block_group20
     	),
 	cds as (
@@ -39,7 +39,7 @@ with
 		select prelim_diz_zone as prelim_diz_zone_plus_1, percentile_50 from analysis.crosswalk_density_summary
 	
 		),
-	diz_with_bonuses_step1 as (
+	diz_bg_with_bonuses_step1 as (
         select
             b.block_group20, 
             b.density_index, 
@@ -54,29 +54,29 @@ with
             case when crosswalk_density > percentile_50 then 1 else 0 end as crosswalk_bonus_step1,
             case when average_comm_stories >= 3 and prelim_diz_zone = 5 and density_index_level = 'very high' and proximity_index_level = 'extreme' then 1 else 0 end as stories_bonus, /*Also creates stories_bonus here*/
 			b.geom
-        from diz_with_acs b
+        from diz_bg_with_av_com_s b
         	left join cds d
             on b.prelim_diz_zone_plus_1 = d.prelim_diz_zone_plus_1
     	),
-	diz_with_bonuses as (
+	diz_bg_with_bonuses as (
 		
 		select block_group20, density_index, proximity_index, density_index_level, proximity_index_level, prelim_diz_zone, crosswalk_density, average_comm_stories,
 		case when prelim_diz_zone in (1,5) then 0 else crosswalk_bonus_step1 end as crosswalk_bonus,
 		stories_bonus, geom
 		
-		from diz_with_bonuses_step1
+		from diz_bg_with_bonuses_step1
 	
 		),
-	diz_with_diz_zone as (
+	diz_bg_with_diz_zone as (
 		
 		select block_group20, density_index, proximity_index, density_index_level, proximity_index_level, prelim_diz_zone, crosswalk_density, average_comm_stories, crosswalk_bonus, stories_bonus, 
 		case when prelim_diz_zone <> 0 then prelim_diz_zone + crosswalk_bonus + stories_bonus else 0 end as diz_zone,
 		geom
 		
-		from diz_with_bonuses
+		from diz_bg_with_bonuses
 	
 		),
-	diz as (
+	diz_block_group as (
         select
             b.block_group20, 
             b.density_index, 
@@ -91,11 +91,11 @@ with
             b.diz_zone,
             d.diz_zone_name,
 			b.geom
-        from diz_with_diz_zone b
+        from diz_bg_with_diz_zone b
         	left join _resources.diz_zone_names d
             on b.diz_zone = d.diz_zone
             order by block_group20
     	)
     
     
-    select row_number() over() as row_number, block_group20, density_index, proximity_index, density_index_level, proximity_index_level, prelim_diz_zone, crosswalk_density, average_comm_stories, crosswalk_bonus, stories_bonus, diz_zone, diz_zone_name, geom from diz
+    select row_number() over() as row_number, block_group20, density_index, proximity_index, density_index_level, proximity_index_level, prelim_diz_zone, crosswalk_density, average_comm_stories, crosswalk_bonus, stories_bonus, diz_zone, diz_zone_name, geom from diz_block_group
