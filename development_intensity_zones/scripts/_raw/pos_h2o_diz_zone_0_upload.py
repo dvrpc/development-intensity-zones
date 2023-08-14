1e09afe73eb91985c87737ffe2356e1951c343e8
ISchwarzenbergDVRPC <ischwarzenberg@dvrpc.org>
2023-08-14 16:33:28 -0400

    Update pos_h2o_diz_zone_0_upload.py

--- development_intensity_zones/scripts/_raw/pos_h2o_diz_zone_0_upload.py
+++ development_intensity_zones/scripts/_raw/pos_h2o_diz_zone_0_upload.py
@@ -11,17 +11,20 @@ db = Database(DATABASE_URL)
 
 
 pos_h2o_diz_zone_0 = gpd.read_file(
-    "G:/Shared drives/Socioeconomic and Land Use Analytics/Tracking Progress/Data Preparation/postgres_db/pos_h2o_diz_zone_0_SHP.shp"
-)  # Brings in the 2D POS and water shapefile with repaired geometries I made
+    "U:_OngoingProjects/CoStar/To be moved later/Transect for AGO/db_exports.gdb",
+    layer="pos_h2o_transect_zone_0_v2",
+)  # Brings in the POS and water feature class Sean Lawrence repaired
 
 pos_h2o_diz_zone_0 = pos_h2o_diz_zone_0.explode(
     ignore_index=True
-)  # Turns multipolygon geometries into regular polygon geometries
+)  # Turns the multipolygon geometries into regular polygon geometries
 
 
+"""
 db.execute(
     "TRUNCATE _raw.pos_h2o_diz_zone_0"
 )  # First wipes out any records in _raw.pos_h2o_diz_zone_0
+"""
 
 db.import_dataframe(
     pos_h2o_diz_zone_0,
