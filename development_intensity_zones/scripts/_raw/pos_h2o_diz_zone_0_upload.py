523e3ac7f6d8bfc787c75059843a090226ff2384
ISchwarzenbergDVRPC <ischwarzenberg@dvrpc.org>
2023-08-17 16:59:16 -0400

    Update pos_h2o_diz_zone_0_upload.py

--- development_intensity_zones/scripts/_raw/pos_h2o_diz_zone_0_upload.py
+++ development_intensity_zones/scripts/_raw/pos_h2o_diz_zone_0_upload.py
@@ -11,13 +11,15 @@ db = Database(DATABASE_URL)
 
 
 pos_h2o_diz_zone_0 = gpd.read_file(
-    "U:_OngoingProjects/CoStar/To be moved later/Transect for AGO/db_exports.gdb",
-    layer="pos_h2o_transect_zone_0_v2",
-)  # Brings in the POS and water feature class Sean Lawrence repaired
+    "U:/_OngoingProjects/CoStar/To be moved later/Transect for AGO/pos_source.gdb",
+    layer="pos_h2o_exploded",
+)  # Brings in the POS and water feature class Sean Lawrence made
 
-pos_h2o_diz_zone_0 = pos_h2o_diz_zone_0.explode(
-    ignore_index=True
-)  # Turns the multipolygon geometries into regular polygon geometries
+pos_h2o_diz_zone_0.insert(
+    0, "dissolve_field", 1
+)  # This and the next command dissolve all the individual polygons in pos_h2o_diz_zone_0 so that it's just 1 big polygon of POS and water
+
+pos_h2o_diz_zone_0 = pos_h2o_diz_zone_0.dissolve(by="dissolve_field")
 
 
 """
