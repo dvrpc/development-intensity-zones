328eba20d0c52695992a77b7c2a0daf14874c702
ISchwarzenbergDVRPC <ischwarzenberg@dvrpc.org>
2023-08-17 15:38:57 -0400

    Update not_in_costar_upload.py

--- development_intensity_zones/scripts/_raw/not_in_costar_upload.py
+++ development_intensity_zones/scripts/_raw/not_in_costar_upload.py
@@ -1,6 +1,6 @@
 """
 BEN SAID HOW WE MAY NEED TO REEVALUATE THE NEED FOR THE _raw.not_in_costar RECORDS BASED ON ANY NEW ADDITIONS TO COSTAR DATA IN FUTURE DOWNLOADS. WE DOWNLOAD A SNAPSHOT OF COSTAR DATA EVERY 6 MONTHS (AFTER 1ST AND 3RD QUARTER OF EACH YEAR) - This script uploads the feature class, THAT BEN GRUSWITZ MANUALLY CREATED, containing AS EXAMPLES, those 2 really big Costar properties, that one in University City and the GSK campus in Montco, as _raw.not_in_costar (this way, more similar properties can get added to this in the future if need be)
-
+"""
 
 import geopandas as gpd
 
