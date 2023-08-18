79606fc6bb672b7c4c7b308a95033a5e4c609fb1
ISchwarzenbergDVRPC <ischwarzenberg@dvrpc.org>
2023-08-18 10:01:32 -0400

    Update pos_h2o_diz_zone_0_upload.py

--- development_intensity_zones/scripts/_raw/pos_h2o_diz_zone_0_upload.py
+++ development_intensity_zones/scripts/_raw/pos_h2o_diz_zone_0_upload.py
@@ -12,24 +12,14 @@ db = Database(DATABASE_URL)
 
 pos_h2o_diz_zone_0 = gpd.read_file(
     "U:/_OngoingProjects/CoStar/To be moved later/Transect for AGO/pos_source.gdb",
-    layer="pos_h2o_exploded",
+    layer="pos_union_wDVRPC_hydro1_dissolve",
 )  # Brings in the POS and water feature class Sean Lawrence made
 
-pos_h2o_diz_zone_0.insert(
-    0, "dissolve_field", 1
-)  # This and the next command dissolve all the individual polygons in pos_h2o_diz_zone_0 so that it's just 1 big polygon of POS and water
 
-pos_h2o_diz_zone_0 = pos_h2o_diz_zone_0.dissolve(by="dissolve_field")
-
-
-"""
 db.execute(
     "TRUNCATE _raw.pos_h2o_diz_zone_0"
 )  # First wipes out any records in _raw.pos_h2o_diz_zone_0
-"""
 
-db.import_dataframe(
-    pos_h2o_diz_zone_0,
-    "_raw.pos_h2o_diz_zone_0",
-    df_import_kwargs={"if_exists": "append", "index": False},
-)  # Then repopulates _raw.pos_h2o_diz_zone_0 with the new records
+db.import_geodataframe(
+    pos_h2o_diz_zone_0, "pos_h2o_diz_zone_0", schema="_raw"
+)  # Then repopulates _raw.pos_h2o_diz_zone_0 with the new record(s)
