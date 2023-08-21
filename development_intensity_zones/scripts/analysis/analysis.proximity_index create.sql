20b839114b4ae10beefc1d79291173ca00efa6c8
ISchwarzenbergDVRPC <ischwarzenberg@dvrpc.org>
2023-08-21 11:36:56 -0400

    Update analysis.proximity_index create.sql

--- development_intensity_zones/scripts/analysis/analysis.proximity_index create.sql
+++ development_intensity_zones/scripts/analysis/analysis.proximity_index create.sql
@@ -11,7 +11,7 @@ with
     	case when proximity_index >= (select proximity_index_thresholds from _resources.thresholds where level_code = 3) and proximity_index < (select proximity_index_thresholds from _resources.thresholds where level_code = 4) then (select levels from _resources.thresholds where level_code = 3) else null end as lc_3,
     	case when proximity_index >= (select proximity_index_thresholds from _resources.thresholds where level_code = 4) and proximity_index < (select proximity_index_thresholds from _resources.thresholds where level_code = 5) then (select levels from _resources.thresholds where level_code = 4) else null end as lc_4,
     	case when proximity_index >= (select proximity_index_thresholds from _resources.thresholds where level_code = 5) and proximity_index < (select proximity_index_thresholds from _resources.thresholds where level_code = 6) then (select levels from _resources.thresholds where level_code = 5) else null end as lc_5,
-    	case when proximity_index >= (select proximity_index_thresholds from _resources.thresholds where level_code = 6) then 'level_code 6' else null end as lc_6,
+    	case when proximity_index >= (select proximity_index_thresholds from _resources.thresholds where level_code = 6) then (select levels from _resources.thresholds where level_code = 6) else null end as lc_6,
     	case when proximity_index is null then 'null' else null end as "null"
     	
     	from analysis.proximity_index_step1
