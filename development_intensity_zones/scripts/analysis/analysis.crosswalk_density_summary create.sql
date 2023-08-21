598f9a27a1b8c57900e5f8f0a1f0dfaea0b4b91b
ISchwarzenbergDVRPC <ischwarzenberg@dvrpc.org>
2023-08-21 11:47:38 -0400

    Update analysis.crosswalk_density_summary create.sql

--- development_intensity_zones/scripts/analysis/analysis.crosswalk_density_summary create.sql
+++ development_intensity_zones/scripts/analysis/analysis.crosswalk_density_summary create.sql
@@ -2,9 +2,13 @@ drop view if exists analysis.crosswalk_density_summary;
 
 create view analysis.crosswalk_density_summary as 
 with 
+	diz_bg_s1 as (
+		select prelim_diz_zone, crosswalk_density from analysis.diz_block_group_step1
+		) --Only worked when this was made as a separate object for some reason
+
 	select prelim_diz_zone, 
 	percentile_cont(0.50) within group (order by crosswalk_density asc) as percentile_50  
-	from (select prelim_diz_zone, crosswalk_density from analysis.diz_block_group_step1) 
+	from diz_bg_s1
 	
 	group by prelim_diz_zone
 	order by prelim_diz_zone --Found out how to get the percentile of a column from https://popsql.com/learn-sql/postgresql/how-to-calculate-percentiles-in-postgresql 
