130e8698985ae9ff657651a69c73a49fe299ae14
ISchwarzenbergDVRPC <ischwarzenberg@dvrpc.org>
2023-08-21 13:45:29 -0400

    Update diz_create.py

--- development_intensity_zones/scripts/analysis/diz_create.py
+++ development_intensity_zones/scripts/analysis/diz_create.py
@@ -11,36 +11,32 @@ from development_intensity_zones import Database, DATABASE_URL
 db = Database(DATABASE_URL)
 
 
-diz_bg_1bigpolygon = db.get_geodataframe_from_query(
-    "SELECT 1 AS dissolve, ST_UNION(geom) as geom FROM analysis.diz_block_group GROUP BY dissolve"
-)  # Uses my function to bring in the analysis.diz_block_group feature class, but dissolved into 1 big polygon
+diz_bg = db.get_geodataframe_from_query(
+    "SELECT * FROM analysis.diz_block_group"
+)  # Uses my function to bring in the analysis.diz_block_group feature class, this time just the way it is originally
 
+diz_bg.insert(
+    0, "dissolve", 1
+)  # This and the next command essentially make a copy of diz_big where all the block groups are dissolved into 1 big polygon
+
+diz_bg_dissolved = diz_bg.dissolve(by="dissolve_field")
 
 pos_and_water = db.get_geodataframe_from_query(
     "SELECT * FROM _raw.pos_h2o_diz_zone_0"
 )  # Uses my function to bring in the _raw.pos_h2o_diz_zone_0 feature class, which is the new POS and water data
 
-pos_and_water = pos_and_water.explode(
-    ignore_index=True
-)  # Turns multipolygon geometries into regular polygon geometries
-
-pos_and_water = pos_and_water.dissolve(
-    by="uid"
-)  # Dissolves all the individual polygons in pos_and_water so that it's just 1 big polygon of POS and water
-
-
 pos_and_water_within_region = pos_and_water.clip(
-    diz_bg_1bigpolygon
-)  # Clips pos_and_water to diz_bg_1bigpolygon, so that pos_and_water doesn't go beyond the boundaries of the region
+    diz_bg_dissolved
+)  # Clips pos_and_water to diz_bg_dissolved, so that pos_and_water doesn't go beyond the boundaries of the region
 
-
-diz_bg = db.get_geodataframe_from_query(
-    "SELECT * FROM analysis.diz_block_group"
-)  # Uses my function to bring in the analysis.diz_block_group feature class, this time just the way it is originally
 
 diz = gpd.overlay(
-    pos_and_water, diz_bg, how="intersection"
-)  # Intersects pos_and_water with diz_bg to create the final diz
+    pos_and_water_within_region, diz_bg, how="intersection"
+)  # Intersects pos_and_water_within_region with diz_bg to create the final diz
+
+diz = diz.explode(
+    ignore_index=True
+)  # Turns multipolygon geometries into regular polygon geometries
 
 db.import_geodataframe(
     diz, "diz", schema="analysis"
