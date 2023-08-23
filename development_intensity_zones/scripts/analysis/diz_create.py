abb976450ba3993f29ea9c40ebf8f77b115a83c9
ISchwarzenbergDVRPC <ischwarzenberg@dvrpc.org>
2023-08-23 10:13:23 -0400

    Update diz_create.py

--- development_intensity_zones/scripts/analysis/diz_create.py
+++ development_intensity_zones/scripts/analysis/diz_create.py
@@ -34,34 +34,35 @@ pos_and_water_reg = pos_and_water.clip(
 
 diz_bg_z0 = diz_bg[diz_bg["diz_zone"] == 0]  # Just gets the zone 0 (protected) parts of diz_bg
 
-pos_and_water_reg_sep_from_z0 = gpd.overlay(
+diff1 = gpd.overlay(
     pos_and_water_reg, diz_bg_z0, how="difference"
 )  # Gets the parts of pos_and_water_reg which DON'T overlap with the zone 0 parts of diz_bg
 
+diff1["diz_zone_name"] = "Protected"  # Adds a DIZ zone name column to diff1
 
-diz_bg = diz_bg[
+diff1 = diff1.rename(columns={"zone": "diz_zone"})  # Renames zone to diz_zone in diff1
+
+diff1 = diff1[
     ["diz_zone", "diz_zone_name", "geom"]
-]  # Just keeps the columns I want from diz_bg that I still need, and in the order I want them to be in
+]  # Just keeps the columns I want from diff1 that I still need, and in the order I want them to be in
 
-diz_bg = diz_bg.dissolve(
+
+diff2 = gpd.overlay(
+    diz_bg_z0, diff1, how="difference"
+)  # Gets the parts of diz_bg_z0 which DON'T overlap with the 1st geographic difference obtained (diff1)
+
+diff2 = diff2[
+    ["diz_zone", "diz_zone_name", "geom"]
+]  # Just keeps the columns I want from the 2nd geographic difference obtained (diff2) that I still need, and in the order I want them to be in
+
+diff2 = diff2.dissolve(
     by=["diz_zone", "diz_zone_name"]
-).reset_index()  # Dissolves diz_bg by 2 columns: diz_zone and diz_zone_name
+).reset_index()  # Dissolves diff2 by 2 columns: diz_zone and diz_zone_name
 
-pos_and_water_reg_sep_from_z0[
-    "diz_zone_name"
-] = "Protected"  # Adds a DIZ zone name column to pos_and_water_reg_sep_from_z0
-
-pos_and_water_reg_sep_from_z0 = pos_and_water_reg_sep_from_z0.rename(
-    columns={"zone": "diz_zone"}
-)  # Renames zone to diz_zone in pos_and_water_reg_sep_from_z0
-
-pos_and_water_reg_sep_from_z0 = pos_and_water_reg_sep_from_z0[
-    ["diz_zone", "diz_zone_name", "geom"]
-]  # Just keeps the columns I want from pos_and_water_reg_sep_from_z0 that I still need, and in the order I want them to be in
 
 diz = pd.concat(
-    [diz_bg, pos_and_water_reg_sep_from_z0]
-)  # Unions/merges/row binds/etc diz_bg and pos_and_water_reg_sep_from_z0 to get what will become analysis.diz
+    [diff2, diff1]
+)  # Unions/merges/row binds/etc diff2 and diff1 to get what will become analysis.diz (I don't think the order of objects matters, it just made more sense in my head spatially to write the order of objects this way)
 
 diz = diz.explode(
     ignore_index=True
