08e8da6474e3632954e37f87c091513b8f67ffc0
ISchwarzenbergDVRPC <ischwarzenberg@dvrpc.org>
2023-08-24 11:26:49 -0400

    Update shared_gdb_populate.py

--- development_intensity_zones/scripts/_Multiple_Schemas/shared_gdb_populate.py
+++ development_intensity_zones/scripts/_Multiple_Schemas/shared_gdb_populate.py
@@ -31,10 +31,20 @@ fc_names = [
 ]  # Makes a list of the feature class' names
 
 
-[
-    db.get_geodataframe_from_query(f"SELECT * FROM {schema_names[i]}.{fc_names[i]}").to_file(
+for i in list(range(len(schema_names))):
+    fc = db.get_geodataframe_from_query(
+        f"SELECT * FROM {schema_names[i]}.{fc_names[i]}"
+    )  # Reads in the feature class
+
+    if list(fc.geom_type.unique()) == ["Polygon", "MultiPolygon"]:
+        fc = fc.explode(
+            ignore_index=True
+        )  # Turns multipolygon geometries into regular polygon geometries, ONLY if there are multipolygon geometries in the feature class
+
+    else:
+        pass
+
+    fc.to_file(
         "U:/_OngoingProjects/Development_Intensity_Zones/project_output/diz_results.gdb",
         layer=fc_names[i],
-    )
-    for i in list(range(len(schema_names)))
-]  # For each feature class, reads it in and then exports it to the shared GDB, therefore also as a feature class. AND IGNORE ANY WARNING MESSAGES THAT COME UP HERE, THIS COMMAND WORKS
+    )  # Exports the feature class to the shared GDB (therefore also as a feature class). AND IGNORE ANY WARNING MESSAGES THAT COME UP HERE, THIS COMMAND WORKS
